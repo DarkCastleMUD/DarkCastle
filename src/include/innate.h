e471ee7e618e9647abc95163d1143416364f2eab
dcastle <dcastle>
2002-10-03 00:53:49 +0000

    Dwarves get innate infra

--- src/include/innate.h
+++ src/include/innate.h
@@ -8,4 +8,5 @@
 #define SKILL_INNATE_BASE               1500
 #define SKILL_INNATE_FLY                1500
 #define SKILL_INNATE_SNEAK              1501
-#define SKILL_INNATE_MAX                1501
+#define SKILL_INNATE_INFRA              1502
+#define SKILL_INNATE_MAX                1502
--- src/innate.cpp
+++ src/innate.cpp
@@ -37,9 +37,11 @@ extern struct index_data *obj_index;
 // local function declarations
 int do_innate_pixie(CHAR_DATA *ch, char *arg, int cmd);
 int do_innate_hobbit(CHAR_DATA *ch, char *arg, int cmd);
+int do_innate_dwarf(CHAR_DATA *ch, char *arg, int cmd);
 
 int do_innate_fly(CHAR_DATA *ch, char *arg, int cmd);
 int do_innate_sneak(CHAR_DATA *ch, char *arg, int cmd);
+int do_innate_infra(CHAR_DATA *ch, char *arg, int cmd);
 
 ////////////////////////////////////////////////////////////////////////////
 // local definitions
@@ -48,6 +50,7 @@ char * innate_skills[] =
 {
    "innate fly timer",
    "innate sneak timer",
+   "innate infra timer",
    "\n"
 };
 
@@ -59,6 +62,7 @@ int do_innate(CHAR_DATA *ch, char *arg, int cmd)
    switch(GET_RACE(ch)) {
       case RACE_PIXIE:    return do_innate_pixie(ch, arg, cmd);
       case RACE_HOBBIT:   return do_innate_hobbit(ch, arg, cmd);
+      case RACE_DWARVEN:  return do_innate_dwarf(ch, arg, cmd);
       default:
          send_to_char("You do not have any innate powers!\r\n", ch);
          return eSUCCESS;
@@ -115,6 +119,31 @@ int do_innate_hobbit(CHAR_DATA *ch, char *arg, int cmd)
    }
 }
 
+int do_innate_dwarf(CHAR_DATA *ch, char *arg, int cmd)
+{
+   char buf[MAX_INPUT_LENGTH];
+
+   one_argument(arg, buf);
+
+   if(!*buf)
+   {
+      send_to_char("As a dwarf, you have the following abilities:\r\n"
+                   "   infravision\r\n"
+                   "\r\n"
+                   "You can activate your innate ability with innate <skill>.\r\n"
+                   , ch);
+      return eSUCCESS;
+   }
+
+   if(!strcmp(buf, "infravision"))
+      return do_innate_infra(ch, buf, cmd);
+   else
+   {
+      csendf(ch, "You do not know of any '%s' ability.\r\n", buf);
+      return eSUCCESS;
+   }
+}
+
 
 ///////////////////////////////////////////////////////////////////////
 //  actual innate abilities
@@ -142,6 +171,31 @@ int do_innate_fly(CHAR_DATA *ch, char *arg, int cmd)
    affect_to_char(ch, &af);
 
    return spell_fly( ( GET_LEVEL(ch) / 2 ), ch, ch, 0, GET_LEVEL(ch) );
+}
+
+int do_innate_infra(CHAR_DATA *ch, char *arg, int cmd)
+{
+   if(affected_by_spell(ch, SKILL_INNATE_INFRA))
+   {
+      send_to_char("It is still too soon for you to be able to call upon your ancestral powers again.\r\n", ch);
+      return eSUCCESS;
+   }
+
+   if(IS_AFFECTED(ch, AFF_INFRARED))
+   {
+      send_to_char("But you are already seeing in the dark!  Why waste it?\r\n", ch);
+      return eSUCCESS;
+   }
+
+   struct affected_type af;
+   af.type = SKILL_INNATE_INFRA;
+   af.duration = 40;
+   af.modifier = 0;
+   af.location = 0;
+   af.bitvector = 0;
+   affect_to_char(ch, &af);
+
+   return spell_infravision( 4, ch, ch, 0, GET_LEVEL(ch) );
 }
 
 int do_innate_sneak(CHAR_DATA *ch, char *arg, int cmd)
