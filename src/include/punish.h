7f51b2cc2cd32f445fc5ea5c29d4b054caa65336
pirahna <pirahna>
2003-01-22 16:18:10 +0000

    removed "punish_thief" define since it's no longer used

--- src/include/punish.h
+++ src/include/punish.h
@@ -1,7 +1,7 @@
 #ifndef PUNISH_H_
 #define PUNISH_H_
 /************************************************************************
-| $Id: punish.h,v 1.2 2002/06/13 04:41:15 dcastle Exp $
+| $Id: punish.h,v 1.3 2003/01/22 16:18:10 pirahna Exp $
 | punish.h
 | Description: Contains punishment vectors
 */
@@ -11,7 +11,7 @@
 #define PUNISH_LOG          1<<2
 #define PUNISH_FREEZE       1<<3
 #define PUNISH_DENY         1<<4
-#define PUNISH_THIEF        1<<5
+#define PUNISH_UNUSED       1<<5
 #define PUNISH_NONAME       1<<6
 #define PUNISH_SPAMMER      1<<7
 #define PUNISH_STUPID       1<<8
