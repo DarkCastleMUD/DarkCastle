4aebaccde32791117411a76feb17032353a90b7e
urizen <urizen>
2004-05-02 20:02:26 +0000

    *** empty log message ***

--- src/guild.cpp
+++ src/guild.cpp
@@ -1,5 +1,5 @@
 /************************************************************************
-| $Id: guild.cpp,v 1.32 2004/05/02 19:59:20 urizen Exp $
+| $Id: guild.cpp,v 1.33 2004/05/02 20:02:26 urizen Exp $
 | guild.C
 | This contains all the guild commands - practice, gain, etc..
 */
@@ -168,6 +168,8 @@ class_skill_defines * get_skill_list(char_data * ch)
   }
   return skilllist;
 }
+void debug_here()
+{}
 
 int skills_guild(struct char_data *ch, char *arg, struct char_data *owner)
 {
@@ -244,6 +246,7 @@ int skills_guild(struct char_data *ch, char *arg, struct char_data *owner)
 	do_say(owner, "I'm sorry, I can't teach you that.  You'll have to find another trainer.",9);
    else {
       struct skill_quest *sq;
+	debug_here();
      if (IS_SET(classon, 1<<GET_CLASS(ch)) && (sq=find_sq(skillnumber)) != NULL && sq->message)
      {
 	mprog_driver(sq->message, owner, ch, NULL, NULL);
