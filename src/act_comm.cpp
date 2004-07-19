548356bca2d1ba1a2ed99beaa321a78fcb23a0b6
rahz <rahz>
2004-07-19 15:55:04 +0000

    *** empty log message ***

--- src/act_comm.cpp
+++ src/act_comm.cpp
@@ -7,7 +7,7 @@
  *  Performance optimization and bug fixes by MERC Industries.             *
  *  You can use our stuff in any way you like whatsoever so long as ths   *
  *  copyright notice remains intact.  If you like it please drop a line    *
- *  to mec\\@garnet.berkeley.edu.                                            *
+ *  to mec\\@garnet.berkeley.edu.                                          *
  *                                                                         *
  *  This is free software and you are benefitting.  We hope that you       *
  *  share your changes too.  What goes around, comes around.               *
