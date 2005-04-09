e8cc10216d2afd97f6e3c4364fba57a46e2cbcf4
urizen <urizen>
2005-04-09 21:20:47 +0000

    Set headerfile.

---
+++ src/include/set.h
@@ -0,0 +1,24 @@
+/* Set eq stuff. Struct, defines. */
+
+
+struct set_data
+{
+  char *SetName;
+  int vnum[19];
+  char *Set_Wear_Message;
+  char *Set_Remove_Message;
+};
+
+#define BASE_SETS                     1200
+#define SET_MAX 		       1204
+// Eq_sets.
+
+#define SET_SAIYAN 0
+#define SET_VESTMENTS 1
+#define SET_HUNTERS 2
+#define SET_CAPTAINS 3
+#define SET_CELEBRANTS 4
+#define SET_RAGER 5
+
+extern const struct set_data set_list[];
+ 
