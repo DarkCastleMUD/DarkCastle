48058e34355a1c1400c22c1e036376050024e215
shane <shane>
2005-06-15 20:52:34 +0000

    added align to set

--- src/wizard/wiz_108.cpp
+++ src/wizard/wiz_108.cpp
@@ -127,7 +127,7 @@ int do_set(struct char_data *ch, char *argument, int cmd)
         "int","wis","dex","con","gold","exp","mana","hit","move",
         "sessions","alignment","thirst","drunk","full","race",
         "bank", "platinum", "ki", "clan", "saves_base", "hpmeta",
-	"manameta","movemeta","\n"
+	"manameta","movemeta","align","\n"
     };
     struct char_data *vict;
     char name[100], buf2[100], buf[100], help[MAX_STRING_LENGTH];
@@ -563,21 +563,34 @@ int do_set(struct char_data *ch, char *argument, int cmd)
               }
               vict->pcdata->saves_mods[i] = value;
             }
+            break;
 	case 28:
 	{
               GET_HP_METAS(vict) = atoi(buf);
               log(buf2, GET_LEVEL(ch), LOG_GOD); 
 	}
+        break;
 	case 29:
 	{
               GET_MANA_METAS(vict) = atoi(buf);
               log(buf2, GET_LEVEL(ch), LOG_GOD); 
 	}
+        break;
 	case 30:
 	{
               GET_MOVE_METAS(vict) = atoi(buf);
               log(buf2, GET_LEVEL(ch), LOG_GOD); 
 	}
+        break;
+            case 31: /* align */
+            {
+                value = atoi(buf);
+                log(buf2, IMP, LOG_GOD);
+                /* set align of victim */
+                vict->align = value;
+            }       
+            break;
+
         }
     
   send_to_char("Ok.\n\r", ch);
