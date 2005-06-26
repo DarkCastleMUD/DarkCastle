358bddcbbfc9ff32694fffd0e0045ef7881b2143
shane <shane>
2005-06-26 19:40:57 +0000

    logic error, i suck

--- src/nanny.cpp
+++ src/nanny.cpp
@@ -16,7 +16,7 @@
 *                        forbidden names from a file instead of a hard-   *
 *                        coded list.                                      *
 ***************************************************************************/
-/* $Id: nanny.cpp,v 1.101 2005/06/23 18:07:44 shane Exp $ */
+/* $Id: nanny.cpp,v 1.102 2005/06/26 19:40:58 shane Exp $ */
 extern "C" {
 #include <stdio.h>
 #include <stdlib.h>
@@ -1673,7 +1673,7 @@ void update_command_lag_and_poison()
       }
 
       // handle drowning
-      if (!IS_NPC(i) && GET_LEVEL(i) < IMMORTAL && world[i->in_room].sector_type == SECT_UNDERWATER && (!affected_by_spell(i, SPELL_WATER_BREATHING) || !IS_AFFECTED(i, AFF_WATER_BREATHING))) {
+      if (!IS_NPC(i) && GET_LEVEL(i) < IMMORTAL && world[i->in_room].sector_type == SECT_UNDERWATER && (!affected_by_spell(i, SPELL_WATER_BREATHING) && !IS_AFFECTED(i, AFF_WATER_BREATHING))) {
          tmp = GET_MAX_HIT(i) / 5;
          sprintf(log_msg, "%s drowned in room %d.", GET_NAME(i), world[i->in_room].number);
          retval = noncombat_damage(i, tmp,
