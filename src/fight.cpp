df819a4081617566eaca437bb0b01c6defef13e2
dcastle <dcastle>
2006-05-03 18:43:56 +0000

    Memory leak fix.

--- src/fight.cpp
+++ src/fight.cpp
@@ -5,7 +5,7 @@ noncombat_damage() to do noncombat-related * * damage (such as falls, drowning) 
 subbed out a lot of * * the code and revised exp calculations for soloers * * and groups.  * * 12/01/2003 Onager Re-revised group_gain() to divide up
 mob exp among * * groupies * * 12/08/2003 Onager Changed change_alignment() to a simpler algorithm * * with smaller changes in alignment * *
 12/28/2003 Pirahna Changed do_fireshield() to check ch->immune instead * * of just race stuff
-****************************************************************************** */ /* $Id: fight.cpp,v 1.293 2006/05/03 18:40:21 dcastle Exp $ */
+****************************************************************************** */ /* $Id: fight.cpp,v 1.294 2006/05/03 18:43:56 dcastle Exp $ */
 
 extern "C"
 {
@@ -2135,6 +2135,8 @@ void send_damage(char *buf, CHAR_DATA *ch, OBJ_DATA *obj, CHAR_DATA *victim, cha
      else
 	send_message(tokens2, ch, obj, victim, 0, victim);
   }
+  delete tokens;
+  delete tokens2;
 }
 
 
