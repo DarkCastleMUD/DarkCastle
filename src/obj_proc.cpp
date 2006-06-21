2389f52f4a9d306bcec9d891c95342dc02e82d93
apocalypse <apocalypse>
2006-06-21 20:00:22 +0000

    - Changed the vnums for the previous committ for assembly arrow. Apoc.

--- src/obj_proc.cpp
+++ src/obj_proc.cpp
@@ -625,7 +625,7 @@ struct assembler_data gem_data[] = {
    { "You carefully sand down the pointy stick, adding to its excellent form.  Splitting the feathers down, you carefully attach them to the pointy stick.  Finally, you shape the scorpion stinger into a deadly arrowhead and secure it to the front.\r\n",
      "$n sits down with some junk and tries $s hand at fletching.",
      "You don't have all the items required to fletch an arrow.\r\n",
-     { 3155, 3156, 3157, -1, -1, -1, -1, -1, -1, -1, 3154 }
+     { 3185, 3186, 3187, -1, -1, -1, -1, -1, -1, -1, 3188 }
    },
 
    // Item 2, Gaiot key in DK
