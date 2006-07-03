77de3c9c17e6ed248c470ce2ceeb446b83fff4f0
dcastle <dcastle>
2006-07-03 14:29:56 +0000

    Oops, right. Now they won't dodge/parry.

--- src/fight.cpp
+++ src/fight.cpp
@@ -6,7 +6,7 @@ noncombat_damage() to do noncombat-related * * damage (such as falls, drowning) 
 subbed out a lot of * * the code and revised exp calculations for soloers * * and groups.  * * 12/01/2003 Onager Re-revised group_gain() to divide up
 mob exp among * * groupies * * 12/08/2003 Onager Changed change_alignment() to a simpler algorithm * * with smaller changes in alignment * *
 12/28/2003 Pirahna Changed do_fireshield() to check ch->immune instead * * of just race stuff
-****************************************************************************** */ /* $Id: fight.cpp,v 1.340 2006/07/03 14:29:02 urizen Exp $ */
+****************************************************************************** */ /* $Id: fight.cpp,v 1.341 2006/07/03 14:29:56 dcastle Exp $ */
 
 extern "C"
 {
@@ -2625,7 +2625,7 @@ bool check_parry(CHAR_DATA * ch, CHAR_DATA * victim, int attacktype)
      return FALSE;
   if (!modifier && IS_NPC(victim) && (ISSET(victim->mobdata->actflags, ACT_PARRY)))
     modifier = 10;
-  else if (IS_NPC(victim) && !ISSET(victim->mobdata->actflags, ACT_PARRY)))
+  else if (IS_NPC(victim) && !ISSET(victim->mobdata->actflags, ACT_PARRY)) 
 	return FALSE; // damned mobs
 
   modifier += speciality_bonus(ch,attacktype,GET_LEVEL(victim));
