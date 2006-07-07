f4a2db1f58403818a8db19b8592ff1a356dda3c2
urizen <urizen>
2006-07-07 07:22:21 +0000

    Right, forgot to initialize a variable.

--- src/vault.cpp
+++ src/vault.cpp
@@ -202,7 +202,7 @@ void save_vault(char *name) {
 void vault_access(CHAR_DATA *ch, char *who)
 {
   struct vault_access_data *access;
-  struct vault_data *vault;
+  struct vault_data *vault = NULL;
 
   if (!vault && !(vault = has_vault(GET_NAME(ch)))) {
     send_to_char("You don't seem to have a vault.\r\n", ch);
