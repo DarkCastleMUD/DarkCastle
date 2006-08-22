a8dd2c24f1ce254669b018e56cb1d0ec3e7db621
Jared H. Hudson <426967+jhhudso@users.noreply.github.com>
2006-08-22 23:21:38 +0000

    Added log msg to tell us when someone casts a spell on a multi'd char

--- src/spells.cpp
+++ src/spells.cpp
@@ -20,7 +20,7 @@
  *  12/07/2003   Onager   Changed PFE/PFG entries in spell_info[] to allow  *
  *                        casting on others                                 *
  ***************************************************************************/
-/* $Id: spells.cpp,v 1.181 2006/07/16 10:43:55 shane Exp $ */
+/* $Id: spells.cpp,v 1.182 2006/08/22 23:21:38 jhhudso Exp $ */
 
 extern "C"
 {
@@ -1914,6 +1914,14 @@ int do_cast(CHAR_DATA *ch, char *argument, int cmd)
         if (tar_char && !AWAKE(tar_char) && ch->in_room == tar_char->in_room && number(1,5) < 3)
             send_to_char("Your sleep is restless.\r\n",tar_char);
 	 skill_increase_check(ch, spl, learned,500+ spell_info[spl].difficulty);
+
+	 if (tar_char && !IS_NPC(ch) && !IS_NPC(tar_char) && tar_char->desc
+	     && ch->desc && (!strcmp(tar_char->desc->host, ch->desc->host))) {
+	   sprintf( log_buf, "Multi: %s cast spell %d on %s", GET_NAME(ch),
+		    spl,
+		    GET_NAME(tar_char));
+	   log( log_buf, 110, LOG_PLAYER );
+	 }
 
         return ((*spell_info[spl].spell_pointer) (GET_LEVEL(ch), ch, argument, SPELL_TYPE_SPELL, tar_char, tar_obj, learned));
       }
