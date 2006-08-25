0a19ad29db687e6dcc4b5d48ef12e657f19ef9dd
apocalypse <apocalypse>
2006-08-25 21:07:52 +0000

    Fixed spacing in "Sir Sweden".

--- src/who.cpp
+++ src/who.cpp
@@ -1,5 +1,5 @@
 /************************************************************************
-| $Id: who.cpp,v 1.25 2006/08/23 17:57:22 apocalypse Exp $
+| $Id: who.cpp,v 1.26 2006/08/25 21:07:52 apocalypse Exp $
 | who.C
 | Commands for who, maybe? :P
 */
@@ -472,7 +472,7 @@ int do_who(struct char_data *ch, char *argument, int cmd)
 	    }
 	     if (!str_cmp(GET_NAME(i), "Urizen")) {
 		infoField = infoBuf;
-		sprintf(infoBuf, " $B$5Sir Sweden$R  ");
+		sprintf(infoBuf, "  $B$5Sir Sweden$R ");
 	     }
 	     if (!str_cmp(GET_NAME(i), "Dasein")) {
 		infoField = infoBuf;
