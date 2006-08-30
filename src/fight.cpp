c3e0000164f60c073af90be06b1a0265b0472161
Jared H. Hudson <426967+jhhudso@users.noreply.github.com>
2006-08-30 17:27:52 +0000

    can_be_attacked no longer allows a mortal to attack a wizinvis immortal

--- src/fight.cpp
+++ src/fight.cpp
@@ -6,7 +6,7 @@ noncombat_damage() to do noncombat-related * * damage (such as falls, drowning) 
 subbed out a lot of * * the code and revised exp calculations for soloers * * and groups.  * * 12/01/2003 Onager Re-revised group_gain() to divide up
 mob exp among * * groupies * * 12/08/2003 Onager Changed change_alignment() to a simpler algorithm * * with smaller changes in alignment * *
 12/28/2003 Pirahna Changed do_fireshield() to check ch->immune instead * * of just race stuff
-****************************************************************************** */ /* $Id: fight.cpp,v 1.371 2006/08/28 20:09:26 shane Exp $ */
+****************************************************************************** */ /* $Id: fight.cpp,v 1.372 2006/08/30 17:27:52 jhhudso Exp $ */
 
 extern "C"
 {
@@ -4990,6 +4990,10 @@ int can_be_attacked(CHAR_DATA *ch, CHAR_DATA *vict)
   
   if(vict->in_room < 0 || vict->in_room >= top_of_world || 
        ch->in_room < 0 || ch->in_room   >= top_of_world) 
+    return FALSE;
+
+  // Ch should not be able to attack a wizinvis immortal player
+  if (!IS_NPC(vict) && GET_LEVEL(ch) < vict->pcdata->wizinvis)
     return FALSE;
 
   if (IS_NPC(vict))
