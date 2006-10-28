416c438717ec6f8442019113e4ea0e7405b97337
Jared H. Hudson <426967+jhhudso@users.noreply.github.com>
2006-10-28 23:54:28 +0000

    Some changes to blacksteel C syntax

--- src/handler.cpp
+++ src/handler.cpp
@@ -12,7 +12,7 @@
  *  This is free software and you are benefitting.  We hope that you       *
  *  share your changes too.  What goes around, comes around.               *
  ***************************************************************************/
-/* $Id: handler.cpp,v 1.135 2006/10/28 20:33:53 apocalypse Exp $ */
+/* $Id: handler.cpp,v 1.136 2006/10/28 23:54:28 jhhudso Exp $ */
     
 extern "C"
 {
@@ -589,14 +589,16 @@ void add_set_stats(char_data *ch, obj_data *obj, int flag, int pos)
             af.location = APPLY_ARMOR;
             af.modifier = -50;
             affect_to_char(ch, &af);
-            af.location = APPLY_ALL_SAVES;
-            af.modifier = 25
+            af.location = APPLY_SAVES;
+            af.modifier = 25;
+            affect_to_char(ch, &af);
           case SET_BLACKSTEELB:
             af.location = APPLY_ARMOR;
             af.modifier = -50;
             affect_to_char(ch, &af);
-            af.location = APPLY_ALL_SAVES;
-            af.modifier = 25
+            af.location = APPLY_SAVES;
+            af.modifier = 25;
+            affect_to_char(ch, &af);
 	  default:           
 		send_to_char("Tough luck, you completed an unimplemented set. Report what you just wore, eh?\r\n",ch);
 		break;
