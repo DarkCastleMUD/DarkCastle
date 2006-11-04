580c5a24e4e488b48ba91d5951d00e583bc603c7
Jared H. Hudson <426967+jhhudso@users.noreply.github.com>
2006-11-04 14:57:22 +0000

    Changed load to be more descriptive in its logs.

--- src/wizard/wizard.cpp
+++ src/wizard/wizard.cpp
@@ -1,5 +1,5 @@
 /************************************************************************
-| $Id: wizard.cpp,v 1.35 2006/11/04 14:36:24 jhhudso Exp $
+| $Id: wizard.cpp,v 1.36 2006/11/04 14:57:22 jhhudso Exp $
 | wizard.C
 | Description:  Utility functions necessary for wiz commands.
 */
@@ -96,12 +96,24 @@ void do_mload(struct char_data *ch, int rnum, int cnt)
   act("$n has created $N!", ch, 0, mob, TO_ROOM, 0);
   sprintf(buf,"You create %i %s!\n\r",cnt, mob->short_desc);
   send_to_char(buf, ch);
-  sprintf(buf,"%s loads %i %d (%s) at %s.",
-          GET_NAME(ch),
-          cnt,
-	  mob_index[rnum].virt,
-          mob->short_desc,
-          world[ch->in_room].name);
+  if (cnt > 1) {
+    snprintf(buf, 100, "%s loads %i copies of mob %d (%s) at room %d (%s).",
+	     GET_NAME(ch),
+	     cnt,
+	     mob_index[rnum].virt,
+	     mob->short_desc,
+	     world[ch->in_room].number,
+	     world[ch->in_room].name);
+  } else {
+    snprintf(buf, 100, "%s loads %i copy of mob %d (%s) at room %d (%s).",
+	     GET_NAME(ch),
+	     cnt,
+	     mob_index[rnum].virt,
+	     mob->short_desc,
+	     world[ch->in_room].number,
+	     world[ch->in_room].name);
+    
+  }
   log(buf, GET_LEVEL(ch), LOG_GOD);
 }    
 
@@ -129,12 +141,23 @@ void do_oload(struct char_data *ch, int rnum, int cnt)
   act("$n has created $p!", ch, obj, 0, TO_ROOM, 0);
   sprintf(buf, "You create %i %s.\n\r", cnt, obj->short_description);
   send_to_char(buf, ch);
-  sprintf(buf,"%s loads %i %d (%s) at %s.",
-          GET_NAME(ch),
-          cnt,
-	  GET_OBJ_VNUM(obj),
-          obj->short_description,
-          world[ch->in_room].name);
+  if (cnt > 1) {
+    snprintf(buf, 100, "%s loads %i copies of obj %d (%s) at room %d (%s).",
+	     GET_NAME(ch),
+	     cnt,
+	     GET_OBJ_VNUM(obj),
+	     obj->short_description,
+	     world[ch->in_room].number,
+	     world[ch->in_room].name);
+  } else {
+    snprintf(buf, 100, "%s loads %i copy of obj %d (%s) at room %d (%s).",
+	     GET_NAME(ch),
+	     cnt,
+	     GET_OBJ_VNUM(obj),
+	     obj->short_description,
+	     world[ch->in_room].number,
+	     world[ch->in_room].name);
+  }
   log(buf, GET_LEVEL(ch), LOG_GOD);
 }    
 
