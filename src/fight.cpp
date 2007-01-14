34f4e0d8f9ca318171d5dd9ba8a3f4a4ff2ba439
Jared H. Hudson <426967+jhhudso@users.noreply.github.com>
2007-01-14 10:45:18 +0000

    Selected race from natural selection persists pkill

--- src/fight.cpp
+++ src/fight.cpp
@@ -6,7 +6,7 @@ noncombat_damage() to do noncombat-related * * damage (such as falls, drowning) 
 subbed out a lot of * * the code and revised exp calculations for soloers * * and groups.  * * 12/01/2003 Onager Re-revised group_gain() to divide up
 mob exp among * * groupies * * 12/08/2003 Onager Changed change_alignment() to a simpler algorithm * * with smaller changes in alignment * *
 12/28/2003 Pirahna Changed do_fireshield() to check ch->immune instead * * of just race stuff
-****************************************************************************** */ /* $Id: fight.cpp,v 1.404 2007/01/14 04:55:57 jhhudso Exp $ */
+****************************************************************************** */ /* $Id: fight.cpp,v 1.405 2007/01/14 10:45:18 jhhudso Exp $ */
 
 extern "C"
 {
@@ -4803,6 +4803,7 @@ void do_pkill(CHAR_DATA *ch, CHAR_DATA *victim, int type)
        af->type != SKILL_INNATE_TIMER &&
        af->type != SPELL_HOLY_AURA_TIMER &&
        af->type != SPELL_NAT_SELECT_TIMER &&
+       af->type != SKILL_NAT_SELECT &&
        af->type != SPELL_DIV_INT_TIMER &&
        af->type != SPELL_DIV_INT_TIMER2 &&
        af->type != SKILL_CRAZED_ASSAULT &&
