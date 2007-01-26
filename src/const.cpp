4e11a990a7a66b706d55558c0c8f31e30d1953d3
dcastle <dcastle>
2007-01-26 01:52:09 +0000

    New more_flags.

--- src/const.cpp
+++ src/const.cpp
@@ -17,7 +17,7 @@
 /* 12/09/2003   Onager   Added protection from good to cleric and anti    */
 /*                       spell list                                       */
 /**************************************************************************/
-/* $Id: const.cpp,v 1.223 2007/01/20 18:32:15 apocalypse Exp $ */
+/* $Id: const.cpp,v 1.224 2007/01/26 01:52:09 dcastle Exp $ */
 /* I KNOW THESE SHOULD BE SOMEWHERE ELSE -- Morc XXX */
 
 extern "C"
@@ -714,6 +714,7 @@ char *more_obj_bits[] =
     "NO_LOCATE",
     "UNIQUE_SAVE",
     "NPC_CORPSE",
+    "PC_CORPSE_LOOTED",
     "\n"
 };
 
