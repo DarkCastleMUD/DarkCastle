af0cbf811033e199d3722614097e23ec51a09cc0
Jared H. Hudson <426967+jhhudso@users.noreply.github.com>
2007-02-11 12:07:34 +0000

    another fix for potato arena

--- src/objects.cpp
+++ src/objects.cpp
@@ -1,5 +1,5 @@
 /************************************************************************
-| $Id: objects.cpp,v 1.85 2007/02/11 11:44:14 jhhudso Exp $
+| $Id: objects.cpp,v 1.86 2007/02/11 12:07:34 jhhudso Exp $
 | objects.C
 | Description:  Implementation of the things you can do with objects:
 |   wear them, wield them, grab them, drink them, eat them, etc..
@@ -172,7 +172,7 @@ void object_activity()
     struct active_object *active_obj;
     struct active_object *next_obj;
   
-    for(active_obj = &active_head; active_obj && (struct active_object *)0x95959595; active_obj = next_obj) {
+    for(active_obj = &active_head; active_obj && active_obj != (struct active_object *)0x95959595; active_obj = next_obj) {
         next_obj = active_obj->next;
         if(active_obj->obj && active_obj->obj != (struct obj_data *)0x9595959) { 
 	  if (obj_index[active_obj->obj->item_number].non_combat_func)  
