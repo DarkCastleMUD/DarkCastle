f769ea79f98f6fcd0dd72b722202191ef05f960e
dcastle <dcastle>
2007-03-01 19:20:32 +0000

    Some new Chains.

--- src/mob_proc.cpp
+++ src/mob_proc.cpp
@@ -12,7 +12,7 @@
  *  This is free software and you are benefitting.  We hope that you       *
  *  share your changes too.  What goes around, comes around.               *
  ***************************************************************************/
-/* $Id: mob_proc.cpp,v 1.146 2007/02/24 14:37:59 dcastle Exp $ */
+/* $Id: mob_proc.cpp,v 1.147 2007/03/01 19:20:32 dcastle Exp $ */
 #ifdef LEAK_CHECK
 #include <dmalloc.h>
 #endif
@@ -1717,9 +1717,11 @@ static char *dethSayText [ ] =
   "A good icebreaker is to ask people what gender they are.",
   "Have you been beating off?  There's no Honolulu.",  // 2007 Tahoe ski trip
   "I once prayed to god for a bike, but quickly found out he didnt work that way...so I stole a bike and prayed for his forgiveness.",
-  "My god carries a hammer. Your god died nailed to a tree. Any questions?" //Thor did it! Damn those Swedes.
-
-
+  "My god carries a hammer. Your god died nailed to a tree. Any questions?", //Thor did it! Damn those Swedes.
+  "The bible rocks. AFK to go stone some disobedient children to death.",
+  "It looks like English, but I can.t understand a damn word you.re saying.",
+  "I thought I wanted a career; turns out I just wanted a salary.",
+  "Thank you. We're all refreshed and challenged by your unique point of view."
 };
 
 // ENDOFCHAIN
