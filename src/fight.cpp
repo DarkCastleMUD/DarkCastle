33cf042c608b934a9a07024f41a4d7af3a08b96c
Jared H. Hudson <426967+jhhudso@users.noreply.github.com>
2007-05-02 03:13:04 +0000

    trip/bash no longer keep bladeshield from going off

--- src/fight.cpp
+++ src/fight.cpp
@@ -6,7 +6,7 @@ noncombat_damage() to do noncombat-related * * damage (such as falls, drowning) 
 subbed out a lot of * * the code and revised exp calculations for soloers * * and groups.  * * 12/01/2003 Onager Re-revised group_gain() to divide up
 mob exp among * * groupies * * 12/08/2003 Onager Changed change_alignment() to a simpler algorithm * * with smaller changes in alignment * *
 12/28/2003 Pirahna Changed do_fireshield() to check ch->immune instead * * of just race stuff
-****************************************************************************** */ /* $Id: fight.cpp,v 1.441 2007/04/08 23:41:01 apocalypse Exp $ */
+****************************************************************************** */ /* $Id: fight.cpp,v 1.442 2007/05/02 03:13:04 jhhudso Exp $ */
 
 extern "C"
 {
@@ -2775,11 +2775,12 @@ bool check_parry(CHAR_DATA * ch, CHAR_DATA * victim, int attacktype)
   int modifier = 0;  
   if((IS_SET(victim->combat, COMBAT_STUNNED)) ||
     (victim->equipment[WIELD] == NULL) ||
-//    (ch->equipment[WIELD] == NULL && number(1, 101) >= 50) ||
     (IS_NPC(victim) && (!ISSET(victim->mobdata->actflags, ACT_PARRY))) ||
     (IS_SET(victim->combat, COMBAT_STUNNED2)) ||
-    (IS_SET(victim->combat, COMBAT_BASH1)) ||
-    (IS_SET(victim->combat, COMBAT_BASH2)) ||
+    ((IS_SET(victim->combat, COMBAT_BASH1) ||
+       IS_SET(victim->combat, COMBAT_BASH2)) &&
+      !IS_SET(victim->combat, COMBAT_BLADESHIELD1) &&
+      !IS_SET(victim->combat, COMBAT_BLADESHIELD2)) ||
     (IS_SET(victim->combat, COMBAT_SHOCKED)) ||
     (IS_SET(victim->combat, COMBAT_SHOCKED2)) ||
     (IS_AFFECTED(victim, AFF_PARALYSIS)))
