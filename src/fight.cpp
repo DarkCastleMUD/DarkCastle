092ab18f8623da7c2bb8fd8a0ac67c54b40e19be
Jared H. Hudson <426967+jhhudso@users.noreply.github.com>
2007-11-09 18:58:23 +0000

    only set selfpurge when a mob kills itself somehow

--- src/fight.cpp
+++ src/fight.cpp
@@ -20,7 +20,7 @@
  * 12/28/2003 Pirahna Changed do_fireshield() to check ch->immune instead *
  * of just race stuff                                                     *
  **************************************************************************
- * $Id: fight.cpp,v 1.463 2007/11/09 06:03:10 jhhudso Exp $               *
+ * $Id: fight.cpp,v 1.464 2007/11/09 18:58:23 jhhudso Exp $               *
  **************************************************************************/
 
 extern "C"
@@ -4154,12 +4154,12 @@ void raw_kill(CHAR_DATA * ch, CHAR_DATA * victim)
   else make_corpse(victim);
   
   if(IS_NPC(victim)) { 
-    // In the event we just killed ourselves within a mprog or oprog, we don't
-    // want to extract ourselves twice.
-      logf(IMMORTAL, LOG_BUG, "selfpurge on %s to %s", GET_NAME(ch), GET_NAME(victim));
-
     extract_char(victim, TRUE);
-    selfpurge = 1;
+
+    if (ch == victim) {
+	logf(IMMORTAL, LOG_BUG, "selfpurge on %s to %s", GET_NAME(ch), GET_NAME(victim));
+	selfpurge = 1;
+    }
     return;
   }
   
