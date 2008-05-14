36e6c1b51951709598f635bf3cf4321597fc86bb
kkoons <kkoons>
2008-05-14 17:34:00 +0000

    Changed druids resist magic and staunchblood to be target castable (as intended)

--- src/spells.cpp
+++ src/spells.cpp
@@ -20,7 +20,7 @@
  *  12/07/2003   Onager   Changed PFE/PFG entries in spell_info[] to allow  *
  *                        casting on others                                 *
  ***************************************************************************/
-/* $Id: spells.cpp,v 1.236 2008/05/13 03:44:29 kkoons Exp $ */
+/* $Id: spells.cpp,v 1.237 2008/05/14 17:34:00 kkoons Exp $ */
 
 extern "C"
 {
@@ -339,9 +339,9 @@ struct spell_info_type spell_info [] =
 
  { /* 119 */ 12, POSITION_STANDING, 50, TAR_CHAR_ROOM|TAR_SELF_ONLY|TAR_SELF_DEFAULT, cast_shadowslip, SKILL_INCREASE_HARD  },
 
- { /* 120 */ 12, POSITION_STANDING, 33, TAR_CHAR_ROOM|TAR_SELF_ONLY|TAR_SELF_DEFAULT, cast_resist_energy, SKILL_INCREASE_HARD },
+ { /* 120 */ 12, POSITION_STANDING, 33, TAR_CHAR_ROOM|TAR_SELF_DEFAULT, cast_resist_energy, SKILL_INCREASE_HARD },
 
- { /* 121 */ 12, POSITION_STANDING, 20, TAR_CHAR_ROOM|TAR_SELF_ONLY|TAR_SELF_DEFAULT, cast_staunchblood, SKILL_INCREASE_HARD  },
+ { /* 121 */ 12, POSITION_STANDING, 20, TAR_CHAR_ROOM|TAR_SELF_DEFAULT, cast_staunchblood, SKILL_INCREASE_HARD  },
 
  { /* 122 */ 24, POSITION_STANDING, 250, TAR_IGNORE, cast_create_golem, SKILL_INCREASE_EASY },
 
