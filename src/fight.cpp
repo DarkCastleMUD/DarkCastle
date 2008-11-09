ed166087ab108ca2e186782a7746845133c05ac4
kkoons <kkoons>
2008-11-09 21:59:58 +0000

    added comment to explain dam >>= 1

--- src/fight.cpp
+++ src/fight.cpp
@@ -20,7 +20,7 @@
  * 12/28/2003 Pirahna Changed do_fireshield() to check ch->immune instead *
  * of just race stuff                                                     *
  **************************************************************************
- * $Id: fight.cpp,v 1.504 2008/11/09 21:22:15 kkoons Exp $               *
+ * $Id: fight.cpp,v 1.505 2008/11/09 21:59:58 kkoons Exp $               *
  **************************************************************************/
 
 extern "C"
@@ -1515,7 +1515,7 @@ int one_hit(CHAR_DATA *ch, CHAR_DATA *vict, int type, int weapon)
   
   do_combatmastery(ch, vict, weapon);
   if (IS_SET(ch->combat, COMBAT_CRUSH_BLOW))
-     dam >>= 1;
+     dam >>= 1; //dam = dam / 2;
 
   if (w_type == TYPE_HIT && IS_NPC(ch))
   {
