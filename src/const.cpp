045a5e308481f825cbf8a0bef018f172950a6308
kkoons <kkoons>
2008-11-18 14:38:14 +0000

    Changed how movement costs are tracked

--- src/const.cpp
+++ src/const.cpp
@@ -17,7 +17,7 @@
 /* 12/09/2003   Onager   Added protection from good to cleric and anti    */
 /*                       spell list                                       */
 /**************************************************************************/
-/* $Id: const.cpp,v 1.273 2008/11/17 07:36:17 shane Exp $ */
+/* $Id: const.cpp,v 1.274 2008/11/18 14:38:14 kkoons Exp $ */
 /* I KNOW THESE SHOULD BE SOMEWHERE ELSE -- Morc XXX */
 
 extern "C"
@@ -34,6 +34,7 @@ extern "C"
 #include <spells.h>
 #include <levels.h>
 #include <mobile.h>
+
 bestowable_god_commands_type bestowable_god_commands[] =
 {
 { "impchan",	COMMAND_IMP_CHAN, false },
@@ -111,6 +112,66 @@ int wear_corr[] =
   ITEM_WEAR_EAR,
   0
 };
+
+   
+
+
+std::map<int,int> fill_map()
+{
+  std::map<int,int> skill_cost_map;
+  skill_cost_map[SKILL_KICK] = 5;
+  skill_cost_map[SKILL_TRIP] = 7;
+  skill_cost_map[SKILL_RESCUE] = 10;
+  skill_cost_map[SKILL_REDIRECT] = 5;
+  skill_cost_map[SKILL_EAGLE_CLAW] = 12;
+  skill_cost_map[SKILL_QUIVERING_PALM] = 45;
+  skill_cost_map[SKILL_STUN] = 15;
+  skill_cost_map[SKILL_BACKSTAB] = 4;
+  skill_cost_map[SKILL_SNEAK] = 5;
+  skill_cost_map[SKILL_HIDE] = 2;
+  skill_cost_map[SKILL_HARM_TOUCH] = 40;
+  skill_cost_map[SKILL_STALK] = 12;
+  skill_cost_map[SKILL_POCKET] = 15;
+  skill_cost_map[SKILL_STEAL] = 20;
+  skill_cost_map[SKILL_CIRCLE] = 12;
+  skill_cost_map[SKILL_DISARM] = 20;
+  skill_cost_map[SKILL_VITAL_STRIKE] = 15;
+  skill_cost_map[SKILL_EYEGOUGE] = 12;
+  skill_cost_map[SKILL_BLACKJACK] = 4;
+  skill_cost_map[SKILL_CRIPPLE] = 30;
+  skill_cost_map[SKILL_PALM] = 2;
+  skill_cost_map[SKILL_SLIP] = 2;
+  skill_cost_map[SKILL_DECEIT] = 16;
+  skill_cost_map[SKILL_APPRAISE] = 6;
+  skill_cost_map[SKILL_PICK_LOCK] = 3;
+  skill_cost_map[SKILL_ESCAPE] = 25;
+  skill_cost_map[SKILL_LAY_HANDS] = 20;
+  skill_cost_map[SKILL_BASH] = 10;
+  skill_cost_map[SKILL_FOCUSED_REPELANCE] =45 ;
+  skill_cost_map[SKILL_BLOOD_FURY] = 60;
+  skill_cost_map[SKILL_CRAZED_ASSAULT] = 18;
+  skill_cost_map[SKILL_RAGE] = 6;
+  skill_cost_map[SKILL_BERSERK] = 12;
+  skill_cost_map[SKILL_HITALL] = 24;
+  skill_cost_map[SKILL_BULLRUSH] = 20;
+  skill_cost_map[SKILL_KNOCKBACK] = 24;
+  skill_cost_map[SKILL_ARCHERY] = 2;
+  skill_cost_map[SKILL_FORAGE] = 8;
+  skill_cost_map[SKILL_TAME] = 15;
+  skill_cost_map[SKILL_AMBUSH] = 24;
+  skill_cost_map[SKILL_TRACK] = 3;
+  skill_cost_map[SKILL_TACTICS] = 15;
+  skill_cost_map[SKILL_RETREAT] = 15;
+  skill_cost_map[SKILL_DEATHSTROKE] = 20;
+  skill_cost_map[SKILL_BLADESHIELD] = 15;
+  skill_cost_map[SKILL_GUARD] = 5;
+  skill_cost_map[SKILL_PRIMAL_FURY] = 40;
+  return skill_cost_map;
+}
+
+std::map<int, int> skill_cost = fill_map();
+
+
 
 // Obj proc types
 char *obj_types[] = {
@@ -2314,117 +2375,6 @@ ubyte backstab_mult[71] =
 }; 
 
 
-int skill_move_cost[SKILL_MAX-SKILL_BASE + 1] =
-{
-	SKILL_TRIP_MOVES,
-	0, /* 1 */
-	0,
-	SKILL_DISARM_MOVES,
-	0,
-	0,
-	SKILL_DEATHSTROKE_MOVES,
-	SKILL_CIRCLE_MOVES,
-	SKILL_BERSERK_MOVES,
-	SKILL_HEADBUTT_MOVES,
-	0, /* 10 */
-	SKILL_QUIVER_MOVES,
-	SKILL_PALM_MOVES,
-	SKILL_STALK_MOVES,
-	0,
-	0,
-	SKILL_HITALL_MOVES,
-	SKILL_STUN_MOVES,
-	0,
-	0,
-	0, /* 20 */
-	SKILL_REDIRECT_MOVES, 
-	SKILL_AMBUSH_MOVES,
-	SKILL_FORAGE_MOVES,
-	SKILL_TAME_MOVES,
-	SKILL_TRACK_MOVES,
-	0,
-	SKILL_SLIP_MOVES,
-	SKILL_RETREAT_MOVES,
-	SKILL_RAGE_MOVES,
-	SKILL_BATTLECRY_MOVES, /* 30 */
-	SKILL_ARCHERY_MOVES,
-	0,
-	SKILL_LAYHANDS_MOVES,
-	0,
-	0,
-	SKILL_SNEAK_MOVES,
-	SKILL_HIDE_MOVES,
-	SKILL_STEAL_MOVES,
-	SKILL_BACKSTAB_MOVES,
-	SKILL_PICKLOCK_MOVES, /* 40 */
-	SKILL_KICK_MOVES, 
-	SKILL_BASH_MOVES,
-	SKILL_RESCUE_MOVES,
-	SKILL_BLOODFURY_MOVES,
-	0,
-	SKILL_HARMTOUCH_MOVES,
-	0,
-	SKILL_BLADESHIELD_MOVES,
-	SKILL_POCKET_MOVES,
-	SKILL_GUARD_MOVES, /* 50 */
-	0,
-	0,
-	0,
-	SKILL_VITALSTRIKE_MOVES,
-	SKILL_CRAZEDASSAULT_MOVES,
-	0,
-	0,
-	0,
-	0,
-	0, /* 60 */
-	0, 
-	0,
-	0,
-	SKILL_BULLRUSH_MOVES,
-	SKILL_FEROCITY_MOVES,
-	SKILL_TACTICS_MOVES,
-	SKILL_DECEIT_MOVES,
-	0,
-	0,
-	SKILL_EYEGOUGE_MOVES, /* 70 */
-	0, 
-	0,
-	0,
-	0,
-	SKILL_KNOCKBACK_MOVES,
-	0,
-	SKILL_BLACKJACK_MOVES,
-	SKILL_APPRAISE_MOVES,
-	0,
-	0, /* 80 */
-	0, 
-	0,
-	0,
-	0,
-	0,
-	0,
-	0,
-	0,
-	SKILL_CRIPPLE_MOVES,
-	0, /* 90 */
-	0, 
-	0,
-	0,
-	0,
-	0,
-	0,
-	0,
-	0,
-	0,
-	0, /* 100 */
-	0, 
-	0,
-	0,
-	0,
-	0,  /* 105 */
-	SKILL_ESCAPE_MOVES,
-	0
-};
 
 
 int mana_bonus[31] =
