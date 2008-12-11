c4d8f932a65c19b8ac42306cd47aa105a83283e6
kkoons <kkoons>
2008-12-11 07:06:35 +0000

    added weap to wild magic

--- src/fight.cpp
+++ src/fight.cpp
@@ -20,7 +20,7 @@
  * 12/28/2003 Pirahna Changed do_fireshield() to check ch->immune instead *
  * of just race stuff                                                     *
  **************************************************************************
- * $Id: fight.cpp,v 1.515 2008/12/08 03:10:07 jhhudso Exp $               *
+ * $Id: fight.cpp,v 1.516 2008/12/11 07:06:35 kkoons Exp $               *
  **************************************************************************/
 
 extern "C"
@@ -6216,7 +6216,7 @@ int weapon_spells(CHAR_DATA *ch, CHAR_DATA *vict, int weapon)
       retval = cast_create_food(GET_LEVEL(ch), ch, "", 0, vict, weap, wep_skill);
       break;
     case WEP_WILD_MAGIC:
-      retval = cast_wild_magic(GET_LEVEL(ch), ch, "offense", 0, vict, NULL, wep_skill);
+      retval = cast_wild_magic(GET_LEVEL(ch), ch, "offense", 0, vict, weap, wep_skill);
       break;
 /*
     case WEP_THIEF_POISON:
