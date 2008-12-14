d7f1878a3825287385504bb9157dc2cc98ed091b
kkoons <kkoons>
2008-12-14 10:10:15 +0000

    Added a LOT more functionality, error checking

--- src/auction.cpp
+++ src/auction.cpp
@@ -41,7 +41,8 @@ extern struct descriptor_data *descriptor_list;
 enum ListOptions
 {
   LIST_ALL = 0,
-  LIST_MINE
+  LIST_MINE,
+  LIST_PRIVATE
 };
 
 enum AuctionStates
@@ -56,6 +57,7 @@ struct AuctionTicket
   string item_name;
   unsigned int price;
   string seller;
+  string buyer;
   AuctionStates state;
   unsigned int end_time;
 };
@@ -67,7 +69,7 @@ public:
   AuctionHouse();
   ~AuctionHouse();
   void CollectExpired(CHAR_DATA *ch, unsigned int ticket = 0);
-  void AddItem(CHAR_DATA *ch, OBJ_DATA *obj, unsigned int price);
+  void AddItem(CHAR_DATA *ch, OBJ_DATA *obj, unsigned int price, string buyer);
   void CancelItem(CHAR_DATA *ch, unsigned int ticket);
   void BuyItem(CHAR_DATA *ch, unsigned int ticket);
   void ListItems(CHAR_DATA *ch, ListOptions option);  
@@ -105,7 +107,7 @@ void AuctionHouse::CollectExpired(CHAR_DATA *ch, unsigned int ticket)
      Item_it = Items_For_Sale.find(ticket);
      if(Item_it != Items_For_Sale.end())
      {
-       if(Item_it->second.seller.compare(GET_SHORT(ch)))
+       if(Item_it->second.seller.compare(GET_NAME(ch)))
        {
          csendf(ch, "Ticket %d doesn't seem to belong to you.\n\r", ticket);
          return;
@@ -122,7 +124,7 @@ void AuctionHouse::CollectExpired(CHAR_DATA *ch, unsigned int ticket)
   queue<unsigned int> tickets_to_cancel;
   for(Item_it = Items_For_Sale.begin(); Item_it != Items_For_Sale.end(); Item_it++)
   {
-    if(Item_it->second.state == AUC_EXPIRED && !Item_it->second.seller.compare(GET_SHORT(ch)))
+    if(Item_it->second.state == AUC_EXPIRED && !Item_it->second.seller.compare(GET_NAME(ch)))
       tickets_to_cancel.push(Item_it->first);    
   }
   while(!tickets_to_cancel.empty())
@@ -172,6 +174,13 @@ void AuctionHouse::BuyItem(CHAR_DATA *ch, unsigned int ticket)
     csendf(ch, "Ticket number %d doesn't seem to exist.\n\r", ticket);
     return;
   }
+
+  if(!Item_it->second.buyer.empty() && Item_it->second.buyer.compare(GET_NAME(ch)))
+  {
+    csendf(ch, "Ticket number %d is private.\n\r", ticket);
+    return;
+  }
+  
   if(GET_GOLD(ch) < Item_it->second.price)
   {
     csendf(ch, "Ticket number %d costs %d coins, you can't afford that!\n\r", 
@@ -179,26 +188,28 @@ void AuctionHouse::BuyItem(CHAR_DATA *ch, unsigned int ticket)
     return;
   }
   
-
   fee = (unsigned int)((double)Item_it->second.price * 0.025);
 
   if (!(vault = has_vault(Item_it->second.seller.c_str()))) 
   { 
     sprintf(buf, "Major screw up in auction! Couldn't find %s's vault to deposit %d coins!", 
-                    Item_it->second.seller.c_str(), Item_it->second.price-fee);
+                  Item_it->second.seller.c_str(), Item_it->second.price-fee);
     log(buf, IMMORTAL, LOG_BUG);
     return;
   }
+  
    
   GET_GOLD(ch) -= Item_it->second.price;
   vault->gold += (Item_it->second.price - fee);
 
   if((vict = get_active_pc(Item_it->second.seller.c_str()))) 
-    csendf(vict, "Your auction of %s has been purchased.\n\r", Item_it->second.item_name.c_str());
+      csendf(vict, "Your auction of %s has been purchased.\n\r", Item_it->second.item_name.c_str());
 
   sprintf(buf, "Added %d gold from sale of %s.", 
-      Item_it->second.price - fee, Item_it->second.item_name.c_str());
+        Item_it->second.price - fee, Item_it->second.item_name.c_str());
   vault_log(buf, Item_it->second.seller.c_str());
+ 
+
   int rnum = real_object(Item_it->second.vitem);
 
   if(rnum < 0)
@@ -247,7 +258,7 @@ void AuctionHouse::CancelItem(CHAR_DATA *ch, unsigned int ticket)
     csendf(ch, "Ticket number %d doesn't seem to exist.\n\r", ticket);
     return;
   }
-  if(Item_it->second.seller.compare(GET_SHORT(ch)))
+  if(Item_it->second.seller.compare(GET_NAME(ch)))
   {
     csendf(ch, "Ticket number %d doesn't belong to you.\n\r", ticket);
     return;
@@ -275,8 +286,24 @@ void AuctionHouse::CancelItem(CHAR_DATA *ch, unsigned int ticket)
     return;
   }
 
-  csendf(ch, "You are given your %s.\n\r", obj->short_description);
+  csendf(ch, "You are returned your %s.\n\r", obj->short_description);
 
+  if(IS_SET(obj->obj_flags.more_flags, ITEM_NO_TRADE))
+  {
+    int rwingding = real_object(27909); //wingding
+    OBJ_DATA *wingding = clone_object(rwingding);
+    if(!wingding)
+    {
+      char buf[MAX_STRING_LENGTH];
+      sprintf(buf, "Major screw up in auction(cancel)! A Wingding for %s could not be created!", 
+                    Item_it->second.seller.c_str());
+      log(buf, IMMORTAL, LOG_BUG);
+      return;
+    }
+    obj_to_char(wingding, ch);
+    csendf(ch, "You are returned your %s.\n\r", wingding->short_description);
+  }
+  
   if(1 != Items_For_Sale.erase(ticket))
   {
     char buf[MAX_STRING_LENGTH];
@@ -298,16 +325,21 @@ void AuctionHouse::ListItems(CHAR_DATA *ch, ListOptions options)
   string output_buf;
   char buf[MAX_STRING_LENGTH];
   
+  send_to_char("Ticket-Seller---------Price-------Status---Item---------------------------\n\r", ch);
   for(i = 0, Item_it = Items_For_Sale.begin(); (i < 50) && (Item_it != Items_For_Sale.end()); Item_it++)
   {
-    if((options == LIST_MINE && !Item_it->second.seller.compare(GET_SHORT(ch)))
-       || (options == LIST_ALL && Item_it->second.state == AUC_FOR_SALE)
+    if(
+       (options == LIST_MINE && !Item_it->second.seller.compare(GET_NAME(ch)))
+       || (options == LIST_ALL && Item_it->second.state == AUC_FOR_SALE 
+           && (Item_it->second.buyer.empty() || !Item_it->second.buyer.compare(GET_NAME(ch))))
+       || (options == LIST_PRIVATE && !Item_it->second.buyer.compare(GET_NAME(ch)))
       )
     {
       i++;
-      sprintf(buf, "\n\r%05d)  Seller: %-18s Item: %-30s\n\r%s Price: %-12d\n\r\n\r", 
-               Item_it->first, Item_it->second.seller.c_str(), Item_it->second.item_name.c_str(), 
-               (Item_it->second.state == AUC_EXPIRED)?"$4EXPIRED$R":"       ", Item_it->second.price);
+      sprintf(buf, "\n\r%05d) %-14s $5%-11d$R %s  %-30s\n\r", 
+               Item_it->first, Item_it->second.seller.c_str(), Item_it->second.price,
+               (Item_it->second.state == AUC_EXPIRED)?"$4EXPIRED$R": "$2ACTIVE$R ", 
+               Item_it->second.item_name.c_str());
       output_buf += buf;
     }
   }
@@ -324,28 +356,47 @@ void AuctionHouse::ListItems(CHAR_DATA *ch, ListOptions options)
 /*
 ADD ITEM
 */
-void AuctionHouse::AddItem(CHAR_DATA *ch, OBJ_DATA *obj, unsigned int price)
+void AuctionHouse::AddItem(CHAR_DATA *ch, OBJ_DATA *obj, unsigned int price, string buyer)
 {
+  char buf[20];
+  strncpy(buf, buyer.c_str(), 19);
+  buf[19] = '\0';
+  unsigned int fee;
+
+  //taken from linkload, formatting of player name
+  char *c;
+  c = buf;
+  *c = UPPER(*c);
+  c++;
+  while(*c) { 
+    *c = LOWER(*c);
+    c++;
+  }
+
   if(!obj)
   {
     send_to_char("You don't seem to have that item.\n\r", ch);
     return;
   }
-  
-  if(price > 2000000000)
+
+  if (IS_SET(obj->obj_flags.extra_flags, ITEM_NOSAVE))
   {
-    send_to_char("Price must be between 0 and 2000000000.\n\r", ch);
+    send_to_char("You can't sell that item!\n\r", ch);
     return;
   }
   
-  AuctionTicket NewTicket;
-  NewTicket.vitem = obj_index[obj->item_number].virt;
-  NewTicket.price = price;
-  NewTicket.state = AUC_FOR_SALE;
-  NewTicket.end_time = time(0) + auction_duration;
-  NewTicket.seller = GET_SHORT(ch);
-  NewTicket.item_name = obj->short_description;
+  if(IS_SET(obj->obj_flags.extra_flags, ITEM_SPECIAL)) 
+  {
+    send_to_char("That sure would be a fucking stupid thing to do.\n\r", ch);
+    return;
+  }
 
+  if(price > 2000000000)
+  {
+    send_to_char("Price must be between 1000 and 2000000000.\n\r", ch);
+    return;
+  }
+  
   unsigned int full_checker = cur_index;
   while(Items_For_Sale.end() != Items_For_Sale.find(++cur_index))
   {
@@ -357,14 +408,52 @@ void AuctionHouse::AddItem(CHAR_DATA *ch, OBJ_DATA *obj, unsigned int price)
       return;
     }
   }
+
+  fee = (unsigned int)((double)price * 0.025); //2.5% fee to list, then 2.5% fee during sale
+  if(GET_GOLD(ch) < fee)
+  {
+    csendf(ch, "You don't have enough gold to pay the %d coin auction fee.\n\r", fee);
+    return;
+  }
+
+  if(IS_SET(obj->obj_flags.more_flags, ITEM_NO_TRADE))
+  {
+    OBJ_DATA *wingding = get_obj_in_list_vis(ch, "wingding", ch->carrying);
+
+    if(!wingding)
+    {
+      send_to_char("You need a wingding to sell no_trade items!\n\r", ch);
+      return;
+    }
+    extract_obj(wingding);
+  }
+
+  GET_GOLD(ch) -= fee;
+  csendf(ch, "Done! You pay the %d coin tax to auction the item.\n\r", fee);
+ 
+  AuctionTicket NewTicket;
+  NewTicket.vitem = obj_index[obj->item_number].virt;
+  NewTicket.price = price;
+  NewTicket.state = AUC_FOR_SALE;
+  NewTicket.end_time = time(0) + auction_duration;
+  NewTicket.seller = GET_NAME(ch);
+  NewTicket.item_name = obj->short_description;
+  NewTicket.buyer = buf;
+
   Items_For_Sale[cur_index] = NewTicket;
   
-  csendf(ch, "You are now selling %s for %d coins.\n\r", obj->short_description, price);
-
+  if(buyer.empty())
+    csendf(ch, "You are now selling %s for %d coins.\n\r", 
+              obj->short_description, price);
+  else
+  {
+    csendf(ch, "You are now selling %s to %s for %d coins.\n\r", 
+              obj->short_description, buyer.c_str(), price);
+  }
 /*
   char auc_buf[MAX_STRING_LENGTH];
   snprintf(auc_buf, MAX_STRING_LENGTH, "%s is now selling %s for %d coins.",
-                         GET_SHORT(ch), obj->short_description, price);
+                         GET_NAME(ch), obj->short_description, price);
 
 
   act(auc_buf, ch, 0, 0, TO_CHAR, 0);
@@ -395,6 +484,11 @@ void AuctionHouse::AddItem(CHAR_DATA *ch, OBJ_DATA *obj, unsigned int price)
 
 AuctionHouse TheAuctionHouse("lib/auctionhouse");
 
+
+/*
+AUCTION_EXPIRE
+Called externally from heartbeat
+*/
 void auction_expire()
 {
   TheAuctionHouse.CheckExpire();
@@ -407,7 +501,6 @@ int do_vend(CHAR_DATA *ch, char *argument, int cmd)
   char buf[MAX_STRING_LENGTH];
   OBJ_DATA *obj;
   unsigned int price;
-  unsigned int fee;
 
   if(ch->in_room != real_room(THALOS_AUCTION_HOUSE))
   {
@@ -419,17 +512,18 @@ int do_vend(CHAR_DATA *ch, char *argument, int cmd)
 
   if(!*buf)
   {
-    send_to_char("Syntax goes here\n\r", ch);
+    send_to_char("Syntax: vend <buy | sell | list | cancel | collect>\n\r", ch);
     return eSUCCESS;       
 
   }
 
+  /*COLLECT*/
   if(!strcmp(buf, "collect"))
   {
     argument = one_argument(argument, buf);
     if(!*buf)
     {
-      send_to_char("Collect what?\n\r", ch);
+      send_to_char("Collect what?\n\rSyntax: vend collect <all | ticket#>\n\r", ch);
       return eSUCCESS;
     }
     if(!strcmp(buf, "all"))
@@ -443,38 +537,47 @@ int do_vend(CHAR_DATA *ch, char *argument, int cmd)
       return eSUCCESS;
     }
     else
-      send_to_char("Collect \"all\" or ticket number.\n\r", ch);
+      send_to_char("Syntax: vend collect <all | ticket#>\n\r", ch);
     return eSUCCESS;
   }
  
+  /*BUY*/
   if(!strcmp(buf, "buy"))
   {
     argument = one_argument(argument, buf);
     if(!*buf)
     {
-      send_to_char("Buy what?\n\r", ch);
+      send_to_char("Buy what?\n\rSyntax: vend buy <ticket #>\n\r", ch);
       return eSUCCESS;
     }
     TheAuctionHouse.BuyItem(ch, atoi(buf));
     return eSUCCESS;
   }
 
+  /*CANCEL*/
   if(!strcmp(buf, "cancel"))
   {
     argument = one_argument(argument, buf);
     if(!*buf)
     {
-      send_to_char("Cancel what?\n\r", ch);
+      send_to_char("Cancel what?\n\rSyntax: vend cancel <ticket#>\n\r", ch);
       return eSUCCESS;
     }
     TheAuctionHouse.CancelItem(ch, atoi(buf));
     return eSUCCESS;
   }
  
+  /*LIST*/
   if(!strcmp(buf, "list"))
   {
     argument = one_argument(argument, buf);
     if(!*buf)
+    {
+      send_to_char("List what?\n\rSyntax: vend list <all | mine | private>\n\r", ch);
+      return eSUCCESS;
+    }
+
+    if(!strcmp(buf, "all"))
     {
       TheAuctionHouse.ListItems(ch, LIST_ALL);
     }
@@ -482,19 +585,24 @@ int do_vend(CHAR_DATA *ch, char *argument, int cmd)
     {
       TheAuctionHouse.ListItems(ch, LIST_MINE);
     }
+    else if (!strcmp(buf, "private"))
+    {
+      TheAuctionHouse.ListItems(ch, LIST_PRIVATE);
+    }
     else
     {
-      send_to_char("List what?\n\r", ch);
+      send_to_char("List what?\n\rSyntax: vend list <all | mine | private>\n\r", ch);
     }
     return eSUCCESS;
   }
 
+  /*SELL*/
   if(!strcmp(buf, "sell"))
   {
     argument = one_argument(argument, buf);
     if(!*buf)
     {
-      send_to_char("Sell what?\n\r", ch);
+      send_to_char("Sell what?\n\rSyntax: vend sell <item> <price> [person]\n\r", ch);
       return eSUCCESS;
     }  
     obj = get_obj_in_list_vis(ch, buf, ch->carrying);
@@ -510,22 +618,14 @@ int do_vend(CHAR_DATA *ch, char *argument, int cmd)
        return eSUCCESS;
     }
     price = atoi(buf);
-    if(!price)
+    if(price < 1000)
     {
-      send_to_char("You don't want to just give it away for free!\n\r", ch);
+      send_to_char("Minimum sell price is 1000 coins!\n\r", ch);
       return eSUCCESS;
     }
-    fee = (int)((double)price * 0.025); //2.5% fee to list, then 2.5% fee during sale
-    if(GET_GOLD(ch) < fee)
-    {
-      csendf(ch, "You don't have enough gold to pay the %d coin auction fee.\n\r", fee);
-      return eSUCCESS;
-    }
-    GET_GOLD(ch) -= fee;
-
-    csendf(ch, "Done! You pay the %d coin tax to auction the item.\n\r", fee);
-
-    TheAuctionHouse.AddItem(ch, obj, price);
+  
+    argument = one_argument(argument, buf); //private name
+    TheAuctionHouse.AddItem(ch, obj, price, buf);
     return eSUCCESS;    
   }
 
