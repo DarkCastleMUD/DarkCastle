b140a5676c07329ea53afab2f91ab76089816d79
kkoons <kkoons>
2009-03-31 23:03:22 +0000

    Added do_behead

--- src/class/cl_paladin.cpp
+++ src/class/cl_paladin.cpp
@@ -205,3 +205,116 @@ victim, dammsg, "A blinding flash fills the area as life force granted from $n's
    return eSUCCESS;
 }
 
+
+int do_behead(struct char_data *ch, char *argument, int cmd)
+{
+  double modifier = 0.0;
+  int skill = 0;
+  double enemy_hp = 0.0;
+  int chance = 0;
+  int retval = eSUCCESS;
+  char buf[MAX_STRING_LENGTH];
+  CHAR_DATA *vict;
+  extern struct index_data *obj_index;
+  
+  one_argument(argument, buf);
+
+  if(IS_MOB(ch) || GET_LEVEL(ch) >= ARCHANGEL )
+    ;
+  else if(!(skill = has_skill(ch, SKILL_BEHEAD))) 
+  {
+    send_to_char("The closest you'll ever get to 'beheading' is at a brit milah. Mazal tov!\r\n", ch);
+    return eFAILURE;
+  }
+
+  if(!ch->equipment[WIELD] ||!IS_SET(ch->equipment[WIELD]->obj_flags.extra_flags, ITEM_TWO_HANDED))
+  {
+    send_to_char("You need to be wielding a two handed sword to behead!\r\n", ch);
+    return eFAILURE;
+  }
+
+  if (!(vict = get_char_room_vis(ch, buf))) 
+  {
+    if(ch->fighting)
+      vict = ch->fighting;
+    else
+    {
+      send_to_char("Whom do you want behead?\n\r", ch);
+      return eFAILURE;
+    }
+  }
+
+
+
+  if (!charge_moves(ch, SKILL_BEHEAD)) return eSUCCESS;
+ 
+  if(!skill_success(ch,vict,SKILL_BEHEAD))
+  {
+    send_to_char("Your mighty swing goes wild!\r\n", ch);
+    retval = one_hit(ch, vict, SKILL_BEHEAD, FIRST);
+    return retval;
+  }
+
+  modifier = 25 + skill / 2 + GET_ALIGNMENT(ch) / 100;
+  modifier /= 100; //range .15-1.0
+  enemy_hp = (GET_HIT(vict)* 100) / GET_MAX_HIT(vict);
+  enemy_hp /= 100; //range 0-1;
+
+  if(enemy_hp <= 0)
+    enemy_hp == 0.01;
+
+  chance = (int)(modifier / (enemy_hp * enemy_hp));  
+
+  if(chance > 90) 
+    chance = 90;
+
+  if(chance < 0)
+    chance = 0;
+
+  //csendf(ch, "mod: %f, enemy_hp: %f, chance: %d\r\n", modifier, enemy_hp, chance);
+
+  if((number(0,99) < chance) && !IS_SET(vict->immune, ISR_SLASH)) 
+  {
+    if ((
+          (vict->equipment[WEAR_NECK_1] && obj_index[vict->equipment[WEAR_NECK_1]->item_number].virt == 518)
+          || (vict->equipment[WEAR_NECK_2] && obj_index[vict->equipment[WEAR_NECK_2]->item_number].virt == 518)
+        ) 
+	&& !number(0,1))
+    { // tarrasque's leash..
+      act("You attempt to behead $N, but your sword bounces of $S neckwear.",ch, 0, vict, TO_CHAR, 0);
+      act("$n attempts to behead $N, but fails.", ch, 0, vict, TO_ROOM, NOTVICT);
+      act("$n attempts to behead you, but cannot cut through your neckwear.",ch,0,vict,TO_VICT,0);
+      retval = damage(ch,vict,0, TYPE_SLASH, SKILL_BEHEAD,0);
+      return eSUCCESS|retval;
+    }
+
+    if(IS_AFFECTED(vict, AFF_NO_BEHEAD)) 
+    {
+      act("$N deftly dodges your beheading attempt!", ch, 0, vict, TO_CHAR, 0);
+      act("$N deftly dodges $n's attempt to behead $M!", ch, 0, vict, TO_ROOM, NOTVICT);
+      act("You deftly avoid $n's attempt to lop your head off!", ch, 0, vict, TO_VICT, 0);
+      retval = damage(ch,vict,0, TYPE_SLASH, SKILL_BEHEAD,0);
+      return eSUCCESS|retval;
+    }
+
+    act("You feel your life end as $n's sword SLICES YOUR HEAD OFF!", ch, 0, vict, TO_VICT, 0);
+    act("You SLICE $N's head CLEAN OFF $S body!", ch, 0, vict, TO_CHAR, 0);
+    act("$n cleanly slices $N's head off $S body!", ch, 0, vict, TO_ROOM, NOTVICT);
+    GET_HIT(vict) = -20;
+    make_head(vict);
+    group_gain(ch, vict); 
+    fight_kill(ch, vict, TYPE_CHOOSE, 0);
+    return eSUCCESS|eVICT_DIED; /* Zero means kill it! */
+  // it died..
+  } 
+  else 
+  { /* You MISS the fucker! */
+    act("You hear the SWOOSH sound of wind as $n's sword attempts to slice off your head!", ch, 0, vict, TO_VICT, 0);
+    act("You miss your attempt to behead $N.", ch, 0, vict, TO_CHAR, 0);
+    act("$N jumps back as $n makes an attempt to BEHEAD $M!", ch, 0, vict, TO_ROOM, NOTVICT);
+    retval = damage(ch,vict,0, TYPE_SLASH, SKILL_BEHEAD,0);
+  }
+
+  return retval;  
+} 
+
