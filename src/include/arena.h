b86a74d57c745f15e3b74c26bc44e781b7b29b1b
shane <shane>
2009-04-24 21:50:17 +0000

    fun hp arena

--- src/include/arena.h
+++ src/include/arena.h
@@ -2,7 +2,7 @@
 #define ARENA_H_
 
 /************************************************************************
-| $Id: arena.h,v 1.5 2008/12/08 01:33:35 jhhudso Exp $
+| $Id: arena.h,v 1.6 2009/04/24 21:50:17 shane Exp $
 | arena.h
 | This contains the arena stuff
 */
@@ -11,7 +11,7 @@
 #define ARENA_HIGH 14680
 #define ARENA_DEATHTRAP 14680
 
-enum ARENA_TYPE { NORMAL, CHAOS, POTATO, PRIZE };
+enum ARENA_TYPE { NORMAL, CHAOS, POTATO, PRIZE, HP };
 enum ARENA_STATUS { CLOSED, OPENED };
 
 struct _arena {
@@ -19,6 +19,7 @@ struct _arena {
   int high;
   int num;
   int cur_num;
+  int hplimit;
   ARENA_TYPE type;
   ARENA_STATUS status;
 };
