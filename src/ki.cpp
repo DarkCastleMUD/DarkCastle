6fcba3d8a90b08527895265de8caf587f5f39b98
Jared H. Hudson <426967+jhhudso@users.noreply.github.com>
2009-10-30 03:40:31 +0000

    removed DEBUG msgs

--- src/ki.cpp
+++ src/ki.cpp
@@ -3,7 +3,7 @@
  * Morcallen 12/18
  *
  */
-/* $Id: ki.cpp,v 1.84 2009/10/29 14:32:47 jhhudso Exp $ */
+/* $Id: ki.cpp,v 1.85 2009/10/30 03:40:31 jhhudso Exp $ */
 
 extern "C"
 {
@@ -1059,7 +1059,6 @@ int ki_transfer( ubyte level, CHAR_DATA *ch, char *arg, CHAR_DATA *victim)
     GET_KI(ch) -= amount;
     temp = number(amount-amount/10, amount+amount/10);  //+-10%
     temp = (temp * learned) / 100;
-
     GET_KI(victim) += temp;
     if(GET_KI(victim) > GET_MAX_KI(victim)) 
       GET_KI(victim) = GET_MAX_KI(victim);
@@ -1080,7 +1079,6 @@ int ki_transfer( ubyte level, CHAR_DATA *ch, char *arg, CHAR_DATA *victim)
     
     temp = number(amount-amount/10, amount+amount/10) * 2;
     temp = (temp * learned) / 100;
-    csendf(ch, "DEBUG: You healed them for %d mana\r\n", temp);
     GET_MANA(victim) += temp;
     if(GET_MANA(victim) > GET_MAX_MANA(victim)) 
       GET_MANA(victim) = GET_MAX_MANA(victim);
