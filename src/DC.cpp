c48264f0d8060d596433812fe9fc5f0123da4583
Jared H. Hudson <426967+jhhudso@users.noreply.github.com>
2017-12-29 01:01:21 -0600

    new DC class for singlton access to character related lists

---
+++ src/DC.cpp
@@ -0,0 +1,49 @@
+/*
+ * DC.cpp
+ *
+ *  Created on: Dec 24, 2017
+ *      Author: jhhudso
+ *      Based on http://www.cplusplus.com/forum/beginner/152735/#msg792909
+ */
+
+#include "DC.h"
+#include "room.h" // NOWHERE
+
+DC& DC::instance()
+{
+	static DC DC_instance { /* ... */};
+	return DC_instance;
+}
+
+void DC::removeDead(void) {
+	char_data *ch = 0;
+	while (!death_list.empty()) {
+		ch = death_list.front();
+		character_list.erase(ch);
+		shooting_list.erase(ch);
+		dc_free(ch);
+		death_list.pop();
+	}
+}
+
+void DC::handleShooting(void) {
+	unordered_set<char_data *> remove_list;
+
+	for (auto &ch : shooting_list) {
+		// ignore the dead
+		if (ch->in_room == NOWHERE) {
+			continue;
+		}
+
+		if (ch->shotsthisround) {
+			ch->shotsthisround--;
+		} else {
+			remove_list.insert(ch);
+		}
+	}
+
+	for (auto &ch : remove_list) {
+		shooting_list.erase(ch);
+	}
+}
+
---
+++ src/include/DC.h
@@ -0,0 +1,47 @@
+/*
+ * DC.h
+ *
+ *  Created on: Dec 24, 2017
+ *      Author: jhhudso
+ *      Based on http://www.cplusplus.com/forum/beginner/152735/#msg792909
+ */
+
+#ifndef SRC_INCLUDE_DC_H_
+#define SRC_INCLUDE_DC_H_
+#include <set>
+#include <queue>
+#include <unordered_set>
+#include "character.h"
+
+
+using namespace std;
+
+typedef set<char_data *> character_list_t;
+
+class DC
+{
+    // favour reference semantics over pointer semantics
+public:
+	DC( /* ... */) { /* ... */
+	}
+	void removeDead(void);
+	void handleShooting(void);
+
+	static DC& instance();
+
+    character_list_t character_list;
+	queue<char_data *> death_list;
+	unordered_set<char_data *> shooting_list;
+
+
+private:
+
+	DC(const DC&) = delete; // non-copyable
+	DC(DC&&) = delete; // and non-movable
+
+    // as there is only one object, assignment would always be assign to self
+	DC& operator=(const DC&) = delete;
+	DC& operator=(DC&&) = delete;
+};
+
+#endif /* SRC_INCLUDE_DC_H_ */
