28792b71d081014c12f28fd3965d1866196232fc
nbj <nicklas.bjalemark@gmail.com>
2020-08-09 13:20:42 +0000

    Fix for aerial exclusion

--- mirror.sh
+++ mirror.sh
@@ -7,8 +7,8 @@ DESTINATION=/srv/dcastle/lib
 tar cfz "$FILENAME" --exclude=playershop* --exclude=*02300* --exclude=*02399* $DESTINATION/mobs $DESTINATION/zonefiles $DESTINATION/world $DESTINATION/objects $DESTINATION/*index 
 shopt -s extglob
 
-cp $SOURCE/mobs/!(*clan*|*03100*|*03000*|*05000*|*17800*|*3200*|*11300*|*18900*|*0600*|*22701*|*401*).mob $DESTINATION/mobs/
-cp $SOURCE/objects/!(*clan*|*03100*|*03000*|*05000*|*17800*|*3200*|*11300*|*18900*|*0600*|*22701*|*401*).obj $DESTINATION/objects/
-cp $SOURCE/zonefiles/!(*clan*|*03100*|*03000*|*05000*|*17800*|*3200*|*11300*|*18900*|*0600*|*22701*|*401*).zon $DESTINATION/zonefiles/
+cp $SOURCE/mobs/!(*clan*|*03100*|*03000*|*05000*|*17800*|*3200*|*11300*|*18900*|*0600*|*22701*|*00401*).mob $DESTINATION/mobs/
+cp $SOURCE/objects/!(*clan*|*03100*|*03000*|*05000*|*17800*|*3200*|*11300*|*18900*|*0600*|*22701*|*00401*).obj $DESTINATION/objects/
+cp $SOURCE/zonefiles/!(*clan*|*03100*|*03000*|*05000*|*17800*|*3200*|*11300*|*18900*|*0600*|*22701*|*00401*).zon $DESTINATION/zonefiles/
 cp $SOURCE/world/!(*clan*|*03100*|*03001*|*05000*|*17800*|*3200*|*11300*|*18900*|*0600*|*22700*|*00400*).txt $DESTINATION/world/
 cp $SOURCE/*index $DESTINATION
