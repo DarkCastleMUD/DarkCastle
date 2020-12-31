559ac8ab08a0ef23ec3a0f8ddddc26cbb380f3cc
Jared H. Hudson <426967+jhhudso@users.noreply.github.com>
2020-12-31 12:53:40 -0600

    formatting

--- src/connect.h
+++ src/connect.h
@@ -50,48 +50,47 @@ struct stat_shit
   int str[5], tel[5], wis[5], dex[5], con[5];
 };
 
-struct descriptor_data
-{
-    unsigned	descriptor;		/* file descriptor for socket	*/
-    int         desc_num;    
-    char *	name;			/* Copy of the player name	*/
-    char	host[80];              	/* hostname			*/
-    int		connected;		/* mode of 'connectedness'	*/
-    int         web_connected;
-    int		wait;			/* wait for how many loops	*/
-    char *	showstr_head;		/* for paging through texts	*/
-    const char **     showstr_vector;
-    int         showstr_count;
-    int         showstr_page;
-    bool	new_newline;		/* prepend newline in output	*/
+struct descriptor_data {
+  int descriptor; /* file descriptor for socket	*/
+  int desc_num;
+  char *name; /* Copy of the player name	*/
+  char host[80]; /* hostname			*/
+  int connected; /* mode of 'connectedness'	*/
+  int web_connected;
+  int wait; /* wait for how many loops	*/
+  char *showstr_head; /* for paging through texts	*/
+  const char **showstr_vector;
+  int showstr_count;
+  int showstr_page;
+  bool new_newline; /* prepend newline in output	*/
   //  char	**str;			/* for the modify-str system	*/
-    char        **hashstr;
-    char        *astr;
-    int		max_str;		/*	-			*/
-    char	buf[10 * MAX_INPUT_LENGTH];	/* buffer for raw input	*/
-    char	last_input[MAX_INPUT_LENGTH];	/* the last input	*/
-    char *	output;			/* queue of strings to send	*/
-    char inbuf[MAX_RAW_INPUT_LENGTH];
-    TXT_Q	input;			/* queue of unprocessed input	*/
-    CHAR_DATA *	character;		/* linked to char		*/
-    CHAR_DATA *	original;		/* for switch / return		*/
-    struct descriptor_data *snooping; /* Who is this char snooping       */
-    struct descriptor_data *snoop_by; /* And who is snooping this char   */
-    struct descriptor_data *	next;	/* link to next descriptor	*/
-    int         tick_wait;      	/* # ticks desired to wait	*/
-    int         reallythere;            /* Goddamm #&@$*% sig 13 (hack) */
-    int         prompt_mode;
-    txt_block   *large_outbuf;
-    int         bufptr;
-    int         bufspace;
-    char        small_outbuf[SMALL_BUFSIZE];
-    ubyte         idle_tics;
-    time_t      login_time;
-    struct stat_shit * stats;            // for rolling up a char 
+  char **hashstr;
+  char *astr;
+  int max_str; /*	-			*/
+  char buf[10 * MAX_INPUT_LENGTH]; /* buffer for raw input	*/
+  char last_input[MAX_INPUT_LENGTH]; /* the last input	*/
+  char *output; /* queue of strings to send	*/
+  char inbuf[MAX_RAW_INPUT_LENGTH];
+  TXT_Q input; /* queue of unprocessed input	*/
+  CHAR_DATA *character; /* linked to char		*/
+  CHAR_DATA *original; /* for switch / return		*/
+  struct descriptor_data *snooping; /* Who is this char snooping       */
+  struct descriptor_data *snoop_by; /* And who is snooping this char   */
+  struct descriptor_data *next; /* link to next descriptor	*/
+  int tick_wait; /* # ticks desired to wait	*/
+  int reallythere; /* Goddamm #&@$*% sig 13 (hack) */
+  int prompt_mode;
+  txt_block *large_outbuf;
+  int bufptr;
+  int bufspace;
+  char small_outbuf[SMALL_BUFSIZE];
+  ubyte idle_tics;
+  time_t login_time;
+  struct stat_shit *stats;            // for rolling up a char
 
-    char	**strnew;			/* for the modify-str system	*/
-    char        *backstr;
-    int         idle_time; // How long the descriptor has been idle, overall.
+  char **strnew; /* for the modify-str system	*/
+  char *backstr;
+  int idle_time; // How long the descriptor has been idle, overall.
 };
 
 #endif
