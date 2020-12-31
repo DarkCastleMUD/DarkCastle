1fd7e9e69dbb2ca4b4eafcb801ef8ec1aa14070e
Jared H. Hudson <426967+jhhudso@users.noreply.github.com>
2020-12-31 12:54:55 -0600

    added DC::config

--- src/DC.h
+++ src/DC.h
@@ -8,17 +8,28 @@
 
 #ifndef SRC_INCLUDE_DC_H_
 #define SRC_INCLUDE_DC_H_
+
 #include <set>
 #include <queue>
 #include <unordered_set>
+#include <netinet/in.h>
+
 #include "character.h"
 #include "obj.h"
-
+#include "character.h"
+#include "fileinfo.h"
 
 using namespace std;
 
-typedef set<char_data*> character_list_t;
+typedef set<char_data *> character_list_t;
 typedef set<obj_data*> obj_list_t;
+typedef set<int> client_descriptor_list_t;
+typedef set<int> server_descriptor_list_t;
+typedef vector<in_port_t> port_list_t;
+typedef set<char_data *>::iterator character_list_i;
+typedef set<int>::iterator client_descriptor_list_i;
+typedef set<int>::iterator server_descriptor_list_i;
+typedef vector<in_port_t>::iterator port_list_i;
 
 class DC {
   // Favor reference semantics over pointer semantics
@@ -31,11 +42,28 @@ public:
   static string getVersion();
   static string getBuildTime();
 
+  server_descriptor_list_t server_descriptor_list;
+  client_descriptor_list_t client_descriptor_list;
   character_list_t character_list;
   queue<char_data*> death_list;
   obj_list_t active_obj_list;
   obj_list_t obj_free_list;
   unordered_set<char_data*> shooting_list;
+
+  struct config {
+    port_list_t ports;
+    bool allow_imp_password = false;
+    bool verbose_mode = false;
+    bool test_mobs = false;
+    bool test_objs = false;
+    bool test_world = false;
+    bool bport = false;
+    bool check_syntax = false;
+    bool stderr_timestamp = true;
+    string dir = DFLT_DIR;
+  } cf;
+
+  void init_game(void);
 
 private:
 
@@ -47,6 +75,11 @@ private:
   // as there is only one object, assignment would always be assign to self
   DC& operator=(const DC&) = delete;
   DC& operator=(DC&&) = delete;
+
+  void game_loop(void);
+  int init_socket(in_port_t port);
 };
+
+
 
 #endif /* SRC_INCLUDE_DC_H_ */
