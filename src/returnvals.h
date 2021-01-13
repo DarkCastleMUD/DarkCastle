441f16b1c5b27fbec13c76a610d26242e19c8a56
Jared H. Hudson <426967+jhhudso@users.noreply.github.com>
2021-01-12 21:45:04 -0600

    added retval enum eIMMUNE_VICTIM

--- src/returnvals.h
+++ src/returnvals.h
@@ -8,16 +8,17 @@
 #define _RETURNVALS_H_
 
 enum {
-  eFAILURE          = 1,
-  eSUCCESS          = 1<<1,
-  eCH_DIED          = 1<<2,
-  eVICT_DIED        = 1<<3,
-  eINTERNAL_ERROR   = 1<<4,
-  eEXTRA_VALUE 	    = 1<<5, // Added to act like a flag, setting if something
+  eFAILURE          = 1U,
+  eSUCCESS          = 1U<<1,
+  eCH_DIED          = 1U<<2,
+  eVICT_DIED        = 1U<<3,
+  eINTERNAL_ERROR   = 1U<<4,
+  eEXTRA_VALUE 	    = 1U<<5, // Added to act like a flag, setting if something
 		           // Special happened in the function.. (verify_existing_components use at the moment)
-  eEXTRA_VAL2       = 1<<6, // damage() needs two
+  eEXTRA_VAL2       = 1U<<6, // damage() needs two
 
-  eDELAYED_EXEC     = 1<<7 // Mobprogs, MPPAUSE
+  eDELAYED_EXEC     = 1U<<7, // Mobprogs, MPPAUSE
+  eIMMUNE_VICTIM    = 1U<<8  // returned by damage() when attacking somethat's immune to attack
 };
 
 
