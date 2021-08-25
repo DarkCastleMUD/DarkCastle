4a4ad7d8d27c0c95ec555df6371a8c7504e5f4c6
Heaven1 <heaveni@netzero.com>
2021-08-25 00:30:22 -0500

    Update inventory.cpp
    Keyrings?? i have no clue how to add an extra_flag

--- src/inventory.cpp
+++ src/inventory.cpp
@@ -2157,7 +2157,8 @@ int has_key(CHAR_DATA *ch, int key)
         if (obj_index[o->item_number].virt == key)
             return(1);
 	if(IS_SET(o->obj_flags.extra_flags, ITEM_SPECIAL) &&
-        (GET_ITEM_TYPE(o) == ITEM_KEYRING)){
+        (GET_ITEM_TYPE(o) == ITEM_CONTAINER) && 
+	(o->obj_flags.extra_flags, ITEM_KEYRING)){
 	    for(tmp_o = o->contains; tmp_o; tmp_o = blah) {
                 blah = tmp_o->next_content;
 		if (obj_index[tmp_o->item_number].virt == key)
