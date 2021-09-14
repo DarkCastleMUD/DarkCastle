e5a9245e30167c04d3b1dc3c0f28d88f59898290
Jared H. Hudson <426967+jhhudso@users.noreply.github.com>
2021-09-14 00:11:01 -0500

    fix typo

--- src/wiz_102.cpp
+++ src/wiz_102.cpp
@@ -184,7 +184,7 @@ int do_check(struct char_data *ch, char *arg, int cmd) {
 
   if (vict->pcdata && vict->pcdata->multi == true)
   {
-    csendf(ch, "They's multiplayed since logging in.\r\n");
+    csendf(ch, "They've multiplayed since logging in.\r\n");
   } else {
     csendf(ch, "They've not multiplayed since logging in.\r\n");
   }
