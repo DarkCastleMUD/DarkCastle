72082059d06ddb761b8a552f9c00d378e8cd494c
Jared H. Hudson <426967+jhhudso@users.noreply.github.com>
2021-10-17 20:05:00 -0500

    refactor skill_increase_check and remove and remove unused debug_here()

--- src/guild.h
+++ src/guild.h
@@ -14,5 +14,7 @@ int get_max(CHAR_DATA *ch, int skill);
 int guild(struct char_data *ch, struct obj_data *obj, int cmd, char *arg, struct char_data *owner);
 int learn_skill(char_data * ch, int skill, int amount, int maximum);
 class_skill_defines * get_skill_list(char_data * ch);
+int search_skills(char * arg, class_skill_defines * list_skills);
+int search_skills2(int arg, class_skill_defines * list_skills);
 
 #endif /* GUILD_H_ */
