16f2afcdd0bc6764006844ec0d9afc761134fc86
Jared H. Hudson <426967+jhhudso@users.noreply.github.com>
2021-12-01 00:04:50 -0600

    added struct class_data

---
+++ src/class.cpp
@@ -0,0 +1,18 @@
+#include "class.h"
+
+classes_t classes = {
+    {"UNDEFINED", "undefined","Und", false,0,0,0,0,0},
+    {"Mage", "mage","Mag", true,0,0,0,15,0},
+    {"Cleric", "cleric","Cle", true,0,0,0,0,15},
+    {"Thief", "thief","Thi", true,0,15,0,0,0},
+    {"Warrior", "warrior","War", true,15,0,0,0,0},
+    {"Anti-Paladin", "anti-paladin","Ant", true,0,14,0,14,0},
+    {"Paladin", "paladin","Pal", true,14,0,0,0,14},
+    {"Barbarian", "barbarian","Bar", true,14,0,14,0,0},
+    {"Monk", "monk","Mon", true,0,0,14,0,14},
+    {"Ranger", "ranger","Ran", true,0,14,14,0,0},
+    {"Bard", "bard","Brd", true,0,0,14,14,0},
+    {"Druid", "druid","Drd", true,0,0,0,14,14},
+    {"Psionic", "psionic","Psi",false,0,0,0,0,0},
+    {"Necromancer", "necromancer","Nec", false,0,0,0,0,0}
+    };
