f5dbb546754a6dd9ee79feb3a604399ae5163d2b
Jared H. Hudson <426967+jhhudso@users.noreply.github.com>
2022-08-30 22:17:31 -0500

    reformatted

--- src/token.cpp
+++ src/token.cpp
@@ -10,31 +10,29 @@
 
 extern "C"
 {
-  #include <string.h>
-  #include <ctype.h>
+#include <string.h>
+#include <ctype.h>
 #include <stdlib.h>
 #include <stdio.h>
 }
 // DarkCastle header files
 #include "db.h"
 #include "room.h"
-#include "character.h"    // CHAR_DATA
-#include "obj.h"          // OBJ_DATA
-#include "levels.h"       // MIN_GOD
-#include "utility.h"      // GET_SHORT, GET_LEVEL, &c
-#include "terminal.h"     // colors
-#include "act.h"          // act flags
-#include "player.h"       // PLR_ANSI PLR_VT100
-#include "handler.h"      // fname()
-#include "token.h"      // fname()
+#include "character.h" // CHAR_DATA
+#include "obj.h"       // OBJ_DATA
+#include "levels.h"    // MIN_GOD
+#include "utility.h"   // GET_SHORT, GET_LEVEL, &c
+#include "terminal.h"  // colors
+#include "act.h"       // act flags
+#include "player.h"    // PLR_ANSI PLR_VT100
+#include "handler.h"   // fname()
+#include "token.h"     // fname()
 #include "machine.h"
 #include "connect.h"
 
 #undef DEBUG_TOKEN
 
 extern CWorld world;
- 
-
 
 /************************************************************************
 | TokenList::TokenList(char *str)
@@ -47,24 +45,22 @@ TokenList::TokenList(const char *str) : head(0), current(0)
 {
 
   int i, stri = 0;
-  const char *strp = str;        // Keeps track of current position
+  const char *strp = str;  // Keeps track of current position
   Token *cur_token;        // Current token
   char temp_str[_MAX_STR]; // Temporary string
 
-
-
-  while(strp[stri] != 0 && stri < _MAX_STR)
+  while (strp[stri] != 0 && stri < _MAX_STR)
   {
-    if(strp[stri] == '$') /* It's a string code */
+    if (strp[stri] == '$') /* It's a string code */
     {
-      temp_str[0] = strp[stri+0];
-      temp_str[1] = strp[stri+1];
+      temp_str[0] = strp[stri + 0];
+      temp_str[1] = strp[stri + 1];
       temp_str[2] = 0;
       stri += 2;
     }
     else /* Go until we find a string code or end */
     {
-      for(i = 0; (strp[stri] != 0) && (strp[stri] != '$') && (stri < _MAX_STR); i++, stri++)
+      for (i = 0; (strp[stri] != 0) && (strp[stri] != '$') && (stri < _MAX_STR); i++, stri++)
       {
         temp_str[i] = strp[stri];
       }
@@ -74,9 +70,9 @@ TokenList::TokenList(const char *str) : head(0), current(0)
     cur_token = new Token(temp_str);
     AddToken(cur_token);
 
-    #ifdef DEBUG_TOKEN
-      cerr << "Added token: " << cur_token->GetBuf() << endl;
-    #endif
+#ifdef DEBUG_TOKEN
+    cerr << "Added token: " << cur_token->GetBuf() << endl;
+#endif
   }
 
   Reset();
@@ -99,10 +95,11 @@ void TokenList::Reset()
 TokenList::~TokenList()
 {
   Token *temp_token, *next_token;
-  
+
   Reset();
 
-  for(temp_token = current; temp_token; temp_token = next_token) {
+  for (temp_token = current; temp_token; temp_token = next_token)
+  {
     next_token = temp_token->Next();
     delete temp_token;
   }
@@ -117,7 +114,7 @@ TokenList::~TokenList()
 */
 void TokenList::AddToken(const Token *new_token)
 {
-  if(current == 0)
+  if (current == 0)
   {
     current = (Token *)new_token;
     head = current;
@@ -146,7 +143,7 @@ void TokenList::Next()
 |   Zero is returned (0) if the string should not be sent -- either
 |   the send_to is asleep or the INVIS_NULL flag was used (for example).
 */
-char * TokenList::Interpret(CHAR_DATA *from, OBJ_DATA *obj, void *vict_obj, CHAR_DATA *send_to, int flags)
+char *TokenList::Interpret(CHAR_DATA *from, OBJ_DATA *obj, void *vict_obj, CHAR_DATA *send_to, int flags)
 {
 
   // Reset the string
@@ -157,234 +154,298 @@ char * TokenList::Interpret(CHAR_DATA *from, OBJ_DATA *obj, void *vict_obj, CHAR
   //--
   // However, we want to go ahead and format what they would get sent so that the
   // MOBProgs will trigger.  Added check to send_to->desc on next line as well since
-  // we no longer check it here.  
+  // we no longer check it here.
   // - pir 11/18/01
-  //if(send_to->desc == 0) return(0);
+  // if(send_to->desc == 0) return(0);
 
   //--
   // A few checks before we bother
   //--
   // If they're not playing and force flag isn't set, don't send
-  if (send_to == nullptr) {
-	  return(0);
+  if (send_to == nullptr)
+  {
+    return (0);
   }
-  if(send_to->desc && send_to->desc->connected != conn::PLAYING && !(flags & FORCE))
-    return(0);
-  if(IS_SET(world[send_to->in_room].room_flags, QUIET) && !(flags & FORCE))
-    return(0);
-  if((send_to == (CHAR_DATA *)vict_obj) && (flags & NOTVICT))
-    return(0);
-  if((GET_LEVEL(send_to) < MIN_GOD) && (flags & GODS))
-    return(0);
-  if((GET_POS(send_to) <= POSITION_SLEEPING) && !(flags & ASLEEP))
-    return(0);
+  if (send_to->desc && send_to->desc->connected != conn::PLAYING && !(flags & FORCE))
+    return (0);
+  if (IS_SET(world[send_to->in_room].room_flags, QUIET) && !(flags & FORCE))
+    return (0);
+  if ((send_to == (CHAR_DATA *)vict_obj) && (flags & NOTVICT))
+    return (0);
+  if ((GET_LEVEL(send_to) < MIN_GOD) && (flags & GODS))
+    return (0);
+  if ((GET_POS(send_to) <= POSITION_SLEEPING) && !(flags & ASLEEP))
+    return (0);
 
   // Ok, now bother
-  for(Reset(); current != 0; Next())
+  for (Reset(); current != 0; Next())
   {
 
-    #ifdef DEBUG_TOKEN
-      if(current->IsAnsi()) 
-	cerr << "ANSI token" << endl;
-      if(current->IsVt100())
-	cerr << "VT100 token" << endl;
-      if(current->IsCode())
-	cerr << "Code token" << endl;
-      if(current->IsText())
-	cerr << "Text token" << endl;
-    #endif
+#ifdef DEBUG_TOKEN
+    if (current->IsAnsi())
+      cerr << "ANSI token" << endl;
+    if (current->IsVt100())
+      cerr << "VT100 token" << endl;
+    if (current->IsCode())
+      cerr << "Code token" << endl;
+    if (current->IsText())
+      cerr << "Text token" << endl;
+#endif
 
-    if(current->IsText())
+    if (current->IsText())
     {
-      #ifdef DEBUG_TOKEN
-	cerr << "It's a text token" << endl;
-      #endif
+#ifdef DEBUG_TOKEN
+      cerr << "It's a text token" << endl;
+#endif
       strcat(interp, current->GetBuf());
     }
-    else if(current->IsAnsi() || current->IsVt100())
+    else if (current->IsAnsi() || current->IsVt100())
     {
-      #ifdef DEBUG_TOKEN
-	cerr << "It's ansi or vt100 code" << endl;
-      #endif
-      if(  IS_MOB(send_to) ||
-           (IS_SET(send_to->pcdata->toggles, PLR_ANSI) && current->IsAnsi()) ||
-           (IS_SET(send_to->pcdata->toggles, PLR_VT100) && current->IsVt100()))
+#ifdef DEBUG_TOKEN
+      cerr << "It's ansi or vt100 code" << endl;
+#endif
+      if (IS_MOB(send_to) ||
+          (IS_SET(send_to->pcdata->toggles, PLR_ANSI) && current->IsAnsi()) ||
+          (IS_SET(send_to->pcdata->toggles, PLR_VT100) && current->IsVt100()))
       {
-        switch(current->GetBuf()[1])
+        switch (current->GetBuf()[1])
         {
-          case '1': strcat(interp, BLUE);    break;
-          case '2': strcat(interp, GREEN);   break;
-          case '3': strcat(interp, CYAN);    break;
-          case '4': strcat(interp, RED);     break;
-          case '5': strcat(interp, YELLOW);  break;
-          case '6': strcat(interp, PURPLE);  break;
-          case '7': strcat(interp, GREY);    break;
-          case '0': strcat(interp, BLACK);   break;
-          case 'B': strcat(interp, BOLD);    break;
-	  case 'L': strcat(interp, FLASH);   break;
-          case 'I': strcat(interp, INVERSE); break;
-          case 'R': strcat(interp, NTEXT);   break;
-          case '*': strcat(interp, EEEE);    break;
-          case '$': strcat(interp, "$"); break; 
-         default:                           break;
+        case '1':
+          strcat(interp, BLUE);
+          break;
+        case '2':
+          strcat(interp, GREEN);
+          break;
+        case '3':
+          strcat(interp, CYAN);
+          break;
+        case '4':
+          strcat(interp, RED);
+          break;
+        case '5':
+          strcat(interp, YELLOW);
+          break;
+        case '6':
+          strcat(interp, PURPLE);
+          break;
+        case '7':
+          strcat(interp, GREY);
+          break;
+        case '0':
+          strcat(interp, BLACK);
+          break;
+        case 'B':
+          strcat(interp, BOLD);
+          break;
+        case 'L':
+          strcat(interp, FLASH);
+          break;
+        case 'I':
+          strcat(interp, INVERSE);
+          break;
+        case 'R':
+          strcat(interp, NTEXT);
+          break;
+        case '*':
+          strcat(interp, EEEE);
+          break;
+        case '$':
+          strcat(interp, "$");
+          break;
+        default:
+          break;
         } // switch
-      } // if they are appropriate
-    } // if it's ansi or vt100
-    else if(current->IsCode())
+      }   // if they are appropriate
+    }     // if it's ansi or vt100
+    else if (current->IsCode())
     {
-      #ifdef DEBUG_TOKEN
-	cerr << "It's a special code" << endl;
-      #endif
-      switch((current->GetBuf())[1])
+#ifdef DEBUG_TOKEN
+      cerr << "It's a special code" << endl;
+#endif
+      switch ((current->GetBuf())[1])
       {
-        case 'n': if(!CAN_SEE(send_to, from,TRUE))
-		  {
-		    if(flags & INVIS_NULL) return(0);
-		    else if(flags & INVIS_VISIBLE)
-		      strcat(interp, GET_SHORT(from));
-		    else strcat(interp, "someone"); 
-		  }
-		  else
-		  {
-        if (GET_SHORT(from))
+      case 'n':
+        if (!CAN_SEE(send_to, from, TRUE))
         {
-          strcat(interp, GET_SHORT(from));
+          if (flags & INVIS_NULL)
+            return (0);
+          else if (flags & INVIS_VISIBLE)
+            strcat(interp, GET_SHORT(from));
+          else
+            strcat(interp, "someone");
         }
-		  }
-		  break; 
-	case 'N': if (!CAN_SEE(send_to, (CHAR_DATA *)vict_obj,TRUE))
-		  {
-		    if(flags & INVIS_NULL) return(0);
-		    else if(flags & INVIS_VISIBLE)
-		      strcat(interp, GET_SHORT((CHAR_DATA *)vict_obj));
-		    else strcat(interp, "someone");
-		  }
-		  else
-		  {
-		    strcat(interp, GET_SHORT((CHAR_DATA *)vict_obj));
-		  }
-		  break;
-        case 'm': strcat(interp, HMHR(from));                     break;
-	case 'M': strcat(interp, HMHR((CHAR_DATA *)vict_obj));    break;
-	case 's': strcat(interp, HSHR(from));                     break;
-	case 'S': strcat(interp, HSHR((CHAR_DATA *)vict_obj));    break;
-	case 'e': strcat(interp, HSSH(from));                     break;
-	case 'E': strcat(interp, HSSH((CHAR_DATA *)vict_obj));    break;
-	case 'o': if(!CAN_SEE_OBJ(send_to, obj))
-		  {
-		    if(flags & INVIS_NULL) return(0);
-		    else if(flags & INVIS_VISIBLE)
-		      strcat(interp, fname(obj->name));
-		    else strcat(interp, "something");
-		  }
-		  else
-		  {
-		    strcat(interp, fname(obj->name));
-		  }
-		  break;
-        case 'O': if(!CAN_SEE_OBJ(send_to, (OBJ_DATA *)vict_obj))
-		  {
-		    if(flags & INVIS_NULL) return(0);
-		    else if(flags & INVIS_VISIBLE)
-		      strcat(interp, fname(((OBJ_DATA *)vict_obj)->name));
-		    else strcat(interp, "something");
-		  }
-		  else
-		  {
-		    strcat(interp, fname(((OBJ_DATA *)vict_obj)->name));
-		  }
-                  break;
-        case 'p': if(!CAN_SEE_OBJ(send_to, obj))
-	          {
-		    if(flags & INVIS_NULL) return(0);
-		    else if(flags & INVIS_VISIBLE)
-		      strcat(interp, obj->short_description);
-		    else strcat(interp, "something");
-		  }
-		  else
-		  {
-		    strcat(interp, obj->short_description);
-		  }
-		  break;
-        case 'P': if(!CAN_SEE_OBJ(send_to, (OBJ_DATA *)vict_obj))
-	          {
-		    if(flags & INVIS_NULL) return(0);
-		    else if(flags & INVIS_VISIBLE)
-		      strcat(interp, ((OBJ_DATA *)vict_obj)->short_description);
-		    else strcat(interp, "something");
-		  }
-		  else
-		  {
-		    strcat(interp, ((OBJ_DATA *)vict_obj)->short_description);
-		  }
-		  break;
+        else
+        {
+          if (GET_SHORT(from))
+          {
+            strcat(interp, GET_SHORT(from));
+          }
+        }
+        break;
+      case 'N':
+        if (!CAN_SEE(send_to, (CHAR_DATA *)vict_obj, TRUE))
+        {
+          if (flags & INVIS_NULL)
+            return (0);
+          else if (flags & INVIS_VISIBLE)
+            strcat(interp, GET_SHORT((CHAR_DATA *)vict_obj));
+          else
+            strcat(interp, "someone");
+        }
+        else
+        {
+          strcat(interp, GET_SHORT((CHAR_DATA *)vict_obj));
+        }
+        break;
+      case 'm':
+        strcat(interp, HMHR(from));
+        break;
+      case 'M':
+        strcat(interp, HMHR((CHAR_DATA *)vict_obj));
+        break;
+      case 's':
+        strcat(interp, HSHR(from));
+        break;
+      case 'S':
+        strcat(interp, HSHR((CHAR_DATA *)vict_obj));
+        break;
+      case 'e':
+        strcat(interp, HSSH(from));
+        break;
+      case 'E':
+        strcat(interp, HSSH((CHAR_DATA *)vict_obj));
+        break;
+      case 'o':
+        if (!CAN_SEE_OBJ(send_to, obj))
+        {
+          if (flags & INVIS_NULL)
+            return (0);
+          else if (flags & INVIS_VISIBLE)
+            strcat(interp, fname(obj->name));
+          else
+            strcat(interp, "something");
+        }
+        else
+        {
+          strcat(interp, fname(obj->name));
+        }
+        break;
+      case 'O':
+        if (!CAN_SEE_OBJ(send_to, (OBJ_DATA *)vict_obj))
+        {
+          if (flags & INVIS_NULL)
+            return (0);
+          else if (flags & INVIS_VISIBLE)
+            strcat(interp, fname(((OBJ_DATA *)vict_obj)->name));
+          else
+            strcat(interp, "something");
+        }
+        else
+        {
+          strcat(interp, fname(((OBJ_DATA *)vict_obj)->name));
+        }
+        break;
+      case 'p':
+        if (!CAN_SEE_OBJ(send_to, obj))
+        {
+          if (flags & INVIS_NULL)
+            return (0);
+          else if (flags & INVIS_VISIBLE)
+            strcat(interp, obj->short_description);
+          else
+            strcat(interp, "something");
+        }
+        else
+        {
+          strcat(interp, obj->short_description);
+        }
+        break;
+      case 'P':
+        if (!CAN_SEE_OBJ(send_to, (OBJ_DATA *)vict_obj))
+        {
+          if (flags & INVIS_NULL)
+            return (0);
+          else if (flags & INVIS_VISIBLE)
+            strcat(interp, ((OBJ_DATA *)vict_obj)->short_description);
+          else
+            strcat(interp, "something");
+        }
+        else
+        {
+          strcat(interp, ((OBJ_DATA *)vict_obj)->short_description);
+        }
+        break;
+      case 'a':
+        switch (*(obj)->name)
+        {
         case 'a':
-			switch(*(obj)->name)
-			{
-			case 'a':
-			case 'A':
-			case 'e':
-			case 'E':
-			case 'i':
-			case 'I':
-			case 'o':
-			case 'O':
-			case 'u':
-			case 'U':
-			case 'y':
-			case 'Y':
-				strcat(interp, "an");
-				break;
-			default:
-				strcat(interp, "a");
-				break;
-			}
-			break;
         case 'A':
-			switch(*((OBJ_DATA *)vict_obj)->name)
-			{
-			case 'a':
-			case 'A':
-			case 'e':
-			case 'E':
-			case 'i':
-			case 'I':
-			case 'o':
-			case 'O':
-			case 'u':
-			case 'U':
-			case 'y':
-			case 'Y':
-				strcat(interp, "an");
-				/* no break */
-			default:
-				strcat(interp, "a");
-			}
-			break;
-        case 'T': strcat(interp, (char *)vict_obj);           break;
-        case 'F': strcat(interp, fname((char *)vict_obj));    break;
-	default : // Illegal code - just output it
-		  strcat(interp, current->GetBuf());
-		  break;
-      } /* switch */
-    } /* if it's a code */
+        case 'e':
+        case 'E':
+        case 'i':
+        case 'I':
+        case 'o':
+        case 'O':
+        case 'u':
+        case 'U':
+        case 'y':
+        case 'Y':
+          strcat(interp, "an");
+          break;
+        default:
+          strcat(interp, "a");
+          break;
+        }
+        break;
+      case 'A':
+        switch (*((OBJ_DATA *)vict_obj)->name)
+        {
+        case 'a':
+        case 'A':
+        case 'e':
+        case 'E':
+        case 'i':
+        case 'I':
+        case 'o':
+        case 'O':
+        case 'u':
+        case 'U':
+        case 'y':
+        case 'Y':
+          strcat(interp, "an");
+          /* no break */
+        default:
+          strcat(interp, "a");
+        }
+        break;
+      case 'T':
+        strcat(interp, (char *)vict_obj);
+        break;
+      case 'F':
+        strcat(interp, fname((char *)vict_obj));
+        break;
+      default: // Illegal code - just output it
+        strcat(interp, current->GetBuf());
+        break;
+      }  /* switch */
+    }    /* if it's a code */
     else // It's unrecognized.  Shouldn't happen.
     {
       log("TokenList::Interpret() sent bad Token!", OVERSEER, LOG_BUG);
     }
-    #ifdef DEBUG_TOKEN
-      cerr << "Output after this loop: " << interp << endl;
-    #endif
-  } /* for loop */    
+#ifdef DEBUG_TOKEN
+    cerr << "Output after this loop: " << interp << endl;
+#endif
+  } /* for loop */
 
   strcat(interp, "\r\n");
 
-  #ifdef DEBUG_TOKEN
-    cerr << "Finished building interp; it is:" << endl;
-    cerr << interp << endl;
-  #endif
+#ifdef DEBUG_TOKEN
+  cerr << "Finished building interp; it is:" << endl;
+  cerr << interp << endl;
+#endif
 
-  return(interp);
+  return (interp);
 }
 
 /************************************************************************
@@ -403,8 +464,8 @@ Token::Token(char *tok_str) : buf(0), type(0), next(0)
 */
 Token::~Token()
 {
-  if(buf)
-    delete [] buf;
+  if (buf)
+    delete[] buf;
   buf = 0;
 }
 
@@ -414,18 +475,18 @@ Token::~Token()
 */
 void Token::SetBuf(char *rhs)
 {
-  if(buf)
-    delete [] buf;
+  if (buf)
+    delete[] buf;
   buf = new char[strlen(rhs) + 1];
 
   strcpy(buf, rhs);
 
-  if(buf[0] != '$')
+  if (buf[0] != '$')
   {
     type = TEXT;
-    #ifdef DEBUG_TOKEN
-      cerr << buf << ": TEXT" << endl;
-    #endif
+#ifdef DEBUG_TOKEN
+    cerr << buf << ": TEXT" << endl;
+#endif
   }
   else
   {
@@ -436,40 +497,44 @@ void Token::SetBuf(char *rhs)
     // appears on the end result output
     // $$ now just prints both since it's handled in 'handle_ansi' -pir 2/14/01
     //--
-    switch(buf[1])
+    switch (buf[1])
     {
-      case '1':
-      case '2':
-      case '3':
-      case '4':
-      case '5':
-      case '6':
-      case '7':
-      case '0':
-      case '*': type = ANSI;
-		#ifdef DEBUG_TOKEN
-		  cerr << buf << ": ANSI" << endl;
-		#endif
-		break;
-      case 'B':
-      case 'I':
-      case 'L':
-      case 'R': type = (VT100|ANSI);
-		#ifdef DEBUG_TOKEN
-		  cerr << buf << ": ANSI|VT100" << endl;
-		#endif
-		break;
-         // we allow $$ to go through now, since it's handled in handle_ansi -pir 2/14/01
-      case '$': type = TEXT; // buf[1] = 0;
-		#ifdef DEBUG_TOKEN
-		  cerr << buf << ": TEXT" << endl;
-		#endif
-		break;
-      default : type = CODE;
-		#ifdef DEBUG_TOKEN
-		  cerr << buf << ": CODE" << endl;
-		#endif
-                break;
+    case '1':
+    case '2':
+    case '3':
+    case '4':
+    case '5':
+    case '6':
+    case '7':
+    case '0':
+    case '*':
+      type = ANSI;
+#ifdef DEBUG_TOKEN
+      cerr << buf << ": ANSI" << endl;
+#endif
+      break;
+    case 'B':
+    case 'I':
+    case 'L':
+    case 'R':
+      type = (VT100 | ANSI);
+#ifdef DEBUG_TOKEN
+      cerr << buf << ": ANSI|VT100" << endl;
+#endif
+      break;
+      // we allow $$ to go through now, since it's handled in handle_ansi -pir 2/14/01
+    case '$':
+      type = TEXT; // buf[1] = 0;
+#ifdef DEBUG_TOKEN
+      cerr << buf << ": TEXT" << endl;
+#endif
+      break;
+    default:
+      type = CODE;
+#ifdef DEBUG_TOKEN
+      cerr << buf << ": CODE" << endl;
+#endif
+      break;
     } /* switch */
-  } /* else */
+  }   /* else */
 }
