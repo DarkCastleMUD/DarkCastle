d0095be531024dc52a6defed393140de54b8a8d9
Jared H. Hudson <426967+jhhudso@users.noreply.github.com>
2022-08-31 19:14:30 -0500

    reformat, change interp to string

--- src/token.h
+++ src/token.h
@@ -4,72 +4,68 @@
 | Token section.  This section begins the Token class and methods.
 */
 
-const int ANSI    = 1<<0;   /* If it's an ansi code */
-const int VT100   = 1<<1;   /* If it's a vt100 code */
-const int CODE    = 1<<2;   /* If it should be interped */
-const int TEXT    = 1<<3;   /* If it's just text */
+const int ANSI = 1 << 0;  /* If it's an ansi code */
+const int VT100 = 1 << 1; /* If it's a vt100 code */
+const int CODE = 1 << 2;  /* If it should be interped */
+const int TEXT = 1 << 3;  /* If it's just text */
 const int _MAX_STR = 2048;
 
 class Token
 {
-  public:
+public:
   //--
   // Public functions
   //--
 
   Token(char *rhs);
   ~Token();
-  int IsAnsi()  { return(type & ANSI);  }
-  int IsVt100() { return(type & VT100);  }
-  int IsCode()  { return(type & CODE);  }
-  int IsText()  { return(type & TEXT);  }
-  const char * GetBuf()    { return(buf); }
+  int IsAnsi() { return (type & ANSI); }
+  int IsVt100() { return (type & VT100); }
+  int IsCode() { return (type & CODE); }
+  int IsText() { return (type & TEXT); }
+  const char *GetBuf() { return (buf); }
   void SetBuf(char *);
-  Token * Next() { return next; }
+  Token *Next() { return next; }
   void Next(Token *n) { next = n; }
 
-  private:
+private:
+  //--
+  // Private variables
+  //--
+  char *buf;   /* Holds the buffer */
+  int type;    /* Holds type of buffer */
+  Token *next; /* Next token in the list */
+};             // end of Token class
+
+class TokenList
+{
+public:
+  //--
+  // Public functions
+  //--
+
+  TokenList(const char *);
+  ~TokenList();
+  string Interpret(CHAR_DATA *from, OBJ_DATA *obj, void *vict_obj, CHAR_DATA *send_to, int flags);
+
+private:
+  //--
+  // Private functions
+  //--
+  void AddToken(const Token *);
+  void Reset();
+  void Next();
+
+  //--
+  // Private constants
+  //--
 
   //--
   // Private variables
   //--
-  char *buf;      /* Holds the buffer */
-  int type;       /* Holds type of buffer */
-  Token *next;    /* Next token in the list */
-}; // end of Token class
-  
-class TokenList
-{
-  public:
-    //--
-    // Public functions
-    //--
+  Token *head;    // Head of the list
+  Token *current; // Current token
+  string interp;  // Interpreted tokens
 
-    TokenList(const char *);
-    ~TokenList();
-    char * Interpret(CHAR_DATA *from, OBJ_DATA *obj, void *vict_obj,
-      CHAR_DATA *send_to, int flags);
-
-  private:
-
-    //--
-    // Private functions
-    //--
-    void AddToken(const Token *);
-    void Reset();
-    void Next();
-
-    //--
-    // Private constants 
-    //--
-
-
-    //--
-    // Private variables
-    //--
-    Token *head;                   // Head of the list
-    Token *current;                // Current token 
-    char interp[_MAX_STR];         // Interpreted tokens
-
-}; // end of TokenList class
+};     // end of TokenList class
 #endif /* TOKEN_H_ */
