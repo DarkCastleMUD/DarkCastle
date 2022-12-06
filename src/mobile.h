78ed0a0ef68e3f483a4f36310870ea165de49010
Jared H. Hudson <426967+jhhudso@users.noreply.github.com>
2022-12-06 00:24:59 -0600

    reformat

--- src/mobile.h
+++ src/mobile.h
@@ -18,67 +18,65 @@
 using namespace std;
 
 void rebuild_rnum_references(int startAt, int type);
-void    mprog_driver            ( char* com_list, char_data* mob,
-                                       char_data* actor, obj_data* obj,
-                                       void* vo, struct mprog_throw_type *thrw, char_data *rndm );
-char *  mprog_next_command      ( char* clist );
-
+void mprog_driver(char *com_list, char_data *mob,
+                  char_data *actor, obj_data *obj,
+                  void *vo, struct mprog_throw_type *thrw, char_data *rndm);
+char *mprog_next_command(char *clist);
 
 bool charExists(char_data *ch);
 
 char *getTemp(char_data *ch, char *name);
 
+#define BASE_STAT 0
 
-#define BASE_STAT      0
-
-//#define NOTHING      0
-#define ACT_SPEC       1
-#define ACT_SENTINEL   2
-#define ACT_SCAVENGER  3
-#define ACT_NOTRACK    4
+// #define NOTHING      0
+#define ACT_SPEC 1
+#define ACT_SENTINEL 2
+#define ACT_SCAVENGER 3
+#define ACT_NOTRACK 4
 #define ACT_NICE_THIEF 5
 #define ACT_AGGRESSIVE 6
-#define ACT_STAY_ZONE  7
-#define ACT_WIMPY      8
-			        /* aggressive only attack sleeping players */
+#define ACT_STAY_ZONE 7
+#define ACT_WIMPY 8
+/* aggressive only attack sleeping players */
 #define ACT_2ND_ATTACK 9
 #define ACT_3RD_ATTACK 10
 #define ACT_4TH_ATTACK 11
-			         /* Each attack bit must be set to get up */
-			         /* 4 attacks                             */
+/* Each attack bit must be set to get up */
+/* 4 attacks                             */
 /*
  * For ACT_AGGRESSIVE_XXX, you must also set ACT_AGGRESSIVE
  * These switches can be combined, if none are selected, then
  * the mobile will attack any alignment (same as if all 3 were set)
  */
-#define ACT_AGGR_EVIL       12
-#define ACT_AGGR_GOOD       13
-#define ACT_AGGR_NEUT       14
-#define ACT_UNDEAD          15
-#define ACT_STUPID          16
-#define ACT_CHARM           17
-#define ACT_HUGE            18
-#define ACT_DODGE           19
-#define ACT_PARRY           20
-#define ACT_RACIST          21
-#define ACT_FRIENDLY        22
-#define ACT_STAY_NO_TOWN    23
-#define ACT_NOMAGIC         24
-#define ACT_DRAINY          25
-#define ACT_BARDCHARM       26
-#define ACT_NOKI            27
-#define ACT_NOMATRIX        28
-#define ACT_BOSS            29
-#define ACT_NOHEADBUTT      30
-#define ACT_NOATTACK        31
-//#define CHECKTHISACT      32 //Do not change unless ASIZE changes
-#define ACT_SWARM           33
-#define ACT_TINY            34
-#define ACT_NODISPEL        35
-#define ACT_POISONOUS       36
-#define ACT_NO_GOLD_BONUS   37
-#define ACT_MAX             37
-//#define CHECKTHISACT      64 //Do not chance unless ASIZE changes
+#define ACT_AGGR_EVIL 12
+#define ACT_AGGR_GOOD 13
+#define ACT_AGGR_NEUT 14
+#define ACT_UNDEAD 15
+#define ACT_STUPID 16
+#define ACT_CHARM 17
+#define ACT_HUGE 18
+#define ACT_DODGE 19
+#define ACT_PARRY 20
+#define ACT_RACIST 21
+#define ACT_FRIENDLY 22
+#define ACT_STAY_NO_TOWN 23
+#define ACT_NOMAGIC 24
+#define ACT_DRAINY 25
+#define ACT_BARDCHARM 26
+#define ACT_NOKI 27
+#define ACT_NOMATRIX 28
+#define ACT_BOSS 29
+#define ACT_NOHEADBUTT 30
+#define ACT_NOATTACK 31
+// #define CHECKTHISACT      32 //Do not change unless ASIZE changes
+#define ACT_SWARM 33
+#define ACT_TINY 34
+#define ACT_NODISPEL 35
+#define ACT_POISONOUS 36
+#define ACT_NO_GOLD_BONUS 37
+#define ACT_MAX 37
+// #define CHECKTHISACT      64 //Do not chance unless ASIZE changes
 
 class SelfPurge
 {
@@ -121,18 +119,18 @@ struct race_data
   char *singular_name;   /* Dwarf, Elf, etc.     */
   string lowercase_name; /* dwarf, elf, etc.     */
   char *plural_name;     /* dwarves, elves, etc. */
-  bool playable;    /* Can a player play as this race? */
-  int32_t body_parts;  /* bitvector for body parts       */
-  int32_t immune;      /* bitvector for immunities       */
-  int32_t resist;      /* bitvector for resistances      */
-  int32_t suscept;     /* bitvector for susceptibilities */
-  int32_t hate_fear;   /* bitvector for hate/fear        */
-  int32_t friendly;    /* bitvector for friendliness     */
-  int  min_weight;      /* min weight */
-  int  max_weight;
-  
-  int  min_height;      
-  int  max_height;
+  bool playable;         /* Can a player play as this race? */
+  int32_t body_parts;    /* bitvector for body parts       */
+  int32_t immune;        /* bitvector for immunities       */
+  int32_t resist;        /* bitvector for resistances      */
+  int32_t suscept;       /* bitvector for susceptibilities */
+  int32_t hate_fear;     /* bitvector for hate/fear        */
+  int32_t friendly;      /* bitvector for friendliness     */
+  int min_weight;        /* min weight */
+  int max_weight;
+
+  int min_height;
+  int max_height;
 
   unsigned min_str;
   unsigned max_str;
@@ -153,9 +151,9 @@ struct race_data
   unsigned min_wis;
   unsigned max_wis;
   int mod_wis;
-  
-  int affects;      /* automatically added affects   */
-  const char *unarmed;    // unarmed attack message
+
+  int affects;         /* automatically added affects   */
+  const char *unarmed; // unarmed attack message
 };
 
 struct mob_matrix_data
@@ -168,10 +166,9 @@ struct mob_matrix_data
   int gold;
 };
 
-
 void translate_value(char *leftptr, char *rightptr, int16_t **vali, uint32_t **valui,
-                char ***valstr, int64_t **vali64, int8_t **valb, char_data *mob, char_data *actor,
-                obj_data *obj, void *vo, char_data *rndm);
+                     char ***valstr, int64_t **vali64, int8_t **valb, char_data *mob, char_data *actor,
+                     obj_data *obj, void *vo, char_data *rndm);
 
 void save_golem_data(char_data *ch);
 void save_charmie_data(char_data *ch);
