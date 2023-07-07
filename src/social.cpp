93199bb6aeae1fcdbad3437d93bb6811c70ef8af
Jared H. Hudson <426967+jhhudso@users.noreply.github.com>
2023-07-06 23:52:36 -0500

    store socials in a QMap

--- src/social.cpp
+++ src/social.cpp
@@ -1,11 +1,11 @@
 // social.C
 // Description:  Anything to do with socials
 
-extern "C"
-{
 #include <string.h>
 #include <stdlib.h> // qsort()
-}
+
+#include <QMap>
+
 #include "fileinfo.h" // SOCIAL_FILE
 #include "structs.h"  // MAX_INPUT_LENGTH
 #include "room.h"
@@ -24,22 +24,17 @@ extern "C"
 
 extern CWorld world;
 
-// storage of socials
-struct social_messg *soc_mess_list; // head of social array
-int32_t num_socials;                // number of actual socials (50 = 0-49)
-int32_t social_array_size;          // size of actual array (since we allocate in chunks)
+QMap<QString, social_messg> socials = {};
 
-struct social_messg *find_social(string arg);
+bool find_social(QString arg, social_messg &social);
 
-int check_social(Character *ch, string pcomm, int length)
+int check_social(Character *ch, QString social_name, int length)
 {
   string arg = {}, buf = {};
-  struct social_messg *action = {};
+  social_messg action = {};
   Character *vict = {};
 
-  tie(pcomm, arg) = half_chop(pcomm);
-
-  if (!(action = find_social(pcomm)))
+  if (find_social(social_name, action) == false)
   {
     return SOCIAL_false;
   }
@@ -71,7 +66,7 @@ int check_social(Character *ch, string pcomm, int length)
     return SOCIAL_true;
   }
 
-  if (action->char_found)
+  if (action.char_found.isEmpty() == false)
   {
     tie(buf, arg) = half_chop(arg);
   }
@@ -82,48 +77,57 @@ int check_social(Character *ch, string pcomm, int length)
 
   if (buf.empty())
   {
-    if (action->char_no_arg)
+    if (action.char_no_arg.isEmpty() == false)
     {
-      act(action->char_no_arg, ch, 0, 0, TO_CHAR, 0);
+      act(action.char_no_arg, ch, 0, 0, TO_CHAR, 0);
     }
 
-    if (action->others_no_arg)
+    if (action.others_no_arg.isEmpty() == false)
     {
-      act(action->others_no_arg, ch, 0, 0, TO_ROOM, (action->hide) ? INVIS_NULL : 0);
+      act(action.others_no_arg, ch, 0, 0, TO_ROOM, (action.hide) ? INVIS_NULL : 0);
     }
     return SOCIAL_true_WITH_NOISE;
   }
 
   if (!(vict = get_char_room_vis(ch, buf)))
   {
-    if (action->not_found)
+    if (action.not_found.isEmpty() == false)
     {
-      act(action->not_found, ch, 0, 0, TO_CHAR, 0);
+      act(action.not_found, ch, 0, 0, TO_CHAR, 0);
     }
   }
   else if (vict == ch)
   {
-    if (action->char_auto)
-      act(action->char_auto, ch, 0, 0, TO_CHAR, 0);
-    if (action->others_auto)
-      act(action->others_auto, ch, 0, 0, TO_ROOM,
-          (action->hide) ? INVIS_NULL : 0);
+    if (action.char_auto.isEmpty() == false)
+    {
+      act(action.char_auto, ch, 0, 0, TO_CHAR, 0);
+    }
+
+    if (action.others_auto.isEmpty() == false)
+    {
+      act(action.others_auto, ch, 0, 0, TO_ROOM, (action.hide) ? INVIS_NULL : 0);
+    }
   }
-  else if (GET_POS(vict) < action->min_victim_position)
+  else if (GET_POS(vict) < action.min_victim_position)
   {
-    act("$N is not in a proper position for that.",
-        ch, 0, vict, TO_CHAR, 0);
+    act("$N is not in a proper position for that.", ch, 0, vict, TO_CHAR, 0);
   }
   else
   {
-    if (action->char_found)
-      act(action->char_found, ch, 0, vict, TO_CHAR, 0);
-    if (action->others_found)
-      act(action->others_found, ch, 0, vict, TO_ROOM,
-          NOTVICT | ((action->hide) ? INVIS_NULL : 0));
-    if (action->vict_found)
-      act(action->vict_found, ch, 0, vict, TO_VICT,
-          (action->hide) ? INVIS_NULL : 0);
+    if (action.char_found.isEmpty() == false)
+    {
+      act(action.char_found, ch, 0, vict, TO_CHAR, 0);
+    }
+
+    if (action.others_found.isEmpty() == false)
+    {
+      act(action.others_found, ch, 0, vict, TO_ROOM, NOTVICT | ((action.hide) ? INVIS_NULL : 0));
+    }
+
+    if (action.vict_found.isEmpty() == false)
+    {
+      act(action.vict_found, ch, 0, vict, TO_VICT, (action.hide) ? INVIS_NULL : 0);
+    }
   }
 
   return SOCIAL_true_WITH_NOISE;
@@ -150,187 +154,131 @@ char *fread_social_string(FILE *fl)
   return (rslt);
 }
 
-// read one social
-// return true on success
-// return false on 'EOF'
-int read_social_from_file(int32_t num_social, FILE *fl)
+QString fread_social_string(QTextStream &fl)
 {
-  char tmp[MAX_INPUT_LENGTH];
-  int hide, min_pos;
+  QString buf = fl.readLine();
+  if (buf.startsWith('#'))
+  {
+    return "";
+  }
 
-  fscanf(fl, " %s ", tmp);
-  if (feof(fl))
+  if (fl.atEnd())
+  {
+    logentry("Fread_social_string - unexpected EOF.", IMMORTAL, LogChannels::LOG_BUG);
+    return "";
+  }
+
+  return buf;
+}
+
+int read_social_from_file(QTextStream &fl)
+{
+  QString social_name = {};
+  int hide = {}, min_pos = {};
+
+  fl >> social_name;
+  if (fl.atEnd() || social_name.startsWith('#'))
+  {
     return false;
-  fscanf(fl, " %d %d \n", &hide, &min_pos);
+  }
+  fl >> hide >> min_pos;
+  fl.skipWhiteSpace();
 
+  social_messg social;
   // read strings that will always be there
-  soc_mess_list[num_social].name = str_dup(tmp);
-  soc_mess_list[num_social].hide = hide;
-  soc_mess_list[num_social].min_victim_position = min_pos;
-  soc_mess_list[num_social].char_no_arg = fread_social_string(fl);
-  soc_mess_list[num_social].others_no_arg = fread_social_string(fl);
-  soc_mess_list[num_social].char_found = fread_social_string(fl);
-  // if no char_found, then the social is done, and the ones below won't be there
-  if (!soc_mess_list[num_social].char_found)
-    return true;
-  soc_mess_list[num_social].others_found = fread_social_string(fl);
-  soc_mess_list[num_social].vict_found = fread_social_string(fl);
-  soc_mess_list[num_social].not_found = fread_social_string(fl);
-  soc_mess_list[num_social].char_auto = fread_social_string(fl);
-  soc_mess_list[num_social].others_auto = fread_social_string(fl);
+  social.name = social_name.toLower();
+  social.hide = hide;
+  social.min_victim_position = min_pos;
+  social.char_no_arg = fread_social_string(fl);
+  social.others_no_arg = fread_social_string(fl);
+  social.char_found = fread_social_string(fl);
+
+  // if char_found is not empty then continue reading
+  if (social.char_found.isEmpty() == false)
+  {
+    social.others_found = fread_social_string(fl);
+    social.vict_found = fread_social_string(fl);
+    social.not_found = fread_social_string(fl);
+    social.char_auto = fread_social_string(fl);
+    social.others_auto = fread_social_string(fl);
+  }
+
+  if (socials.contains(social.name))
+  {
+    logf(100, LOG_BUG, QString("socials already contains '%1'").arg(social.name).toStdString().c_str());
+  }
+  else
+  {
+    socials[social.name] = social;
+  }
   return true;
-}
-
-// this function used by qsort to sort the social array
-int compare_social_sort(const void *A, const void *B)
-{
-  int i;
-  for (i = 0;; i++)
-    if (!(*(((social_messg *)A)->name + i) && *(((social_messg *)B)->name + i)))
-      break;
-    else if (*(((social_messg *)A)->name + i) > *(((social_messg *)B)->name + i))
-      return 1;
-    else if (*(((social_messg *)A)->name + i) < *(((social_messg *)B)->name + i))
-      return -1;
-  // Match so far..
-  if (strlen(((social_messg *)A)->name) > strlen(((social_messg *)B)->name))
-    return 1;
-  if (strlen(((social_messg *)A)->name) < strlen(((social_messg *)B)->name))
-    return -1;
-  return 0;
-}
-
-// this function used by qsort to search the social array
-int compare_social_search(const void *A, const void *B)
-{
-  int i;
-  for (i = 0;; i++)
-    if (!(*(((char *)A) + i) && *(((social_messg *)B)->name + i)))
-      break;
-    else if (*(((char *)A) + i) > *(((social_messg *)B)->name + i))
-      return 1;
-    else if (*(((char *)A) + i) < *(((social_messg *)B)->name + i))
-      return -1;
-  // Match so far..
-  if (strlen(((char *)A)) > strlen(((social_messg *)B)->name))
-    return 1;
-  if (strlen(((char *)A)) < strlen(((social_messg *)B)->name))
-    return 0;
-  return 0;
-  //  return len_cmp( (char *) A, ((social_messg *) B)->name );
 }
 
 void boot_social_messages(void)
 {
-  FILE *fl;
+  QFile social_file(SOCIAL_FILE);
 
-  // initialize our array
-  num_socials = 0;
-  social_array_size = 450; // Guess on number of socials.  Closer this is to actual without
-                           // going over saves on memory and boot up speed since we won't
-                           // have to realloc as often.
-#ifdef LEAK_CHECK
-  soc_mess_list = (struct social_messg *)calloc(social_array_size, sizeof(struct social_messg));
-#else
-  soc_mess_list = (struct social_messg *)dc_alloc(social_array_size, sizeof(struct social_messg));
-#endif
-
-  if (!(fl = fopen(SOCIAL_FILE, "r")))
+  if (!social_file.open(QIODevice::ReadOnly | QIODevice::Text))
   {
     perror("Can't open social file in boot_social_messages");
     abort();
   }
+  QTextStream fl(&social_file);
 
   for (;;)
   {
-    // do we have room?
-    if (num_socials >= social_array_size)
+    if (!read_social_from_file(fl))
     {
-      social_array_size += 10;
-      // realloc the list to have enough memory for the new array size
-#ifdef LEAK_CHECK
-      soc_mess_list = (struct social_messg *)realloc(soc_mess_list,
-                                                     (sizeof(struct social_messg) * social_array_size));
-#else
-      soc_mess_list = (struct social_messg *)dc_realloc(soc_mess_list,
-                                                        (sizeof(struct social_messg) * social_array_size));
-#endif
-      // clear the memory we just alloc'd
-      memset((soc_mess_list + num_socials), 0, (sizeof(struct social_messg) * 10));
-    }
-    if (!read_social_from_file(num_socials, fl))
       break;
-    num_socials++;
+    }
   }
-
-  // sort it!
-  qsort(soc_mess_list, num_socials, sizeof(social_messg), compare_social_sort);
-
-  fclose(fl);
 }
 
-struct social_messg *find_social(string arg)
+bool find_social(QString arg, social_messg &target_social)
 {
-  // now uses a linear search
-  int i;
+  for (const social_messg &social : socials)
+  {
+    if (social.name.startsWith(arg, Qt::CaseInsensitive))
+    {
+      qDebug() << social.name << "vs" << arg;
+      target_social = social;
+      return true;
+    };
+  }
 
-  for (i = 1; i < num_socials; i++)
-    if (!compare_social_search((void *)arg.c_str(), (void *)&soc_mess_list[i]))
-      return &soc_mess_list[i];
-
-  return nullptr;
-  //    sprintf(buf + strlen(buf), "%18s", soc_mess_list[i].name);
-
-  //  return (social_messg *) bsearch(arg, soc_mess_list, num_socials, sizeof(struct social_messg), compare_social_search);
+  return false;
 }
 
 void clean_socials_from_memory()
 {
-  for (int i = 0; i < num_socials; i++)
-  {
-    if (soc_mess_list[i].name)
-      dc_free(soc_mess_list[i].name);
-    if (soc_mess_list[i].char_no_arg)
-      dc_free(soc_mess_list[i].char_no_arg);
-    if (soc_mess_list[i].others_no_arg)
-      dc_free(soc_mess_list[i].others_no_arg);
-    if (soc_mess_list[i].char_found)
-      dc_free(soc_mess_list[i].char_found);
-    if (soc_mess_list[i].others_found)
-      dc_free(soc_mess_list[i].others_found);
-    if (soc_mess_list[i].vict_found)
-      dc_free(soc_mess_list[i].vict_found);
-    if (soc_mess_list[i].not_found)
-      dc_free(soc_mess_list[i].not_found);
-    if (soc_mess_list[i].char_auto)
-      dc_free(soc_mess_list[i].char_auto);
-    if (soc_mess_list[i].others_auto)
-      dc_free(soc_mess_list[i].others_auto);
-  }
-
-  dc_free(soc_mess_list);
+  socials = {};
 }
 
 int do_social(Character *ch, char *argument, int cmd)
 {
-  int i;
-  char buf[MAX_STRING_LENGTH];
-  *buf = '\0';
+  QString buf;
 
-  for (i = 1; i < num_socials; i++)
+  uint64_t i = 0;
+
+  for (const social_messg &social : socials)
   {
-    sprintf(buf + strlen(buf), "%18s", soc_mess_list[i].name);
-    if (!(i % 4))
+    buf += QString("%1").arg(social.name, 18);
+
+    if (!(i++ % 4))
     {
-      strcat(buf, "\r\n");
-      send_to_char(buf, ch);
-      *buf = '\0';
+      buf += "\r\n";
+      ch->send(buf);
+      buf = {};
     }
   }
-  if (*buf)
-    send_to_char(buf, ch);
 
-  sprintf(buf, "\r\nCurrent Socials:  %d\r\n", i);
-  send_to_char(buf, ch);
+  if (buf.isEmpty() == false)
+  {
+    ch->send(buf);
+  }
+
+  buf = QString("\r\nCurrent Socials:  %1\r\n").arg(socials.size());
+  ch->send(buf);
+
   return eSUCCESS;
 }
--- src/social.h
+++ src/social.h
@@ -9,25 +9,25 @@
 
 struct social_messg
 {
-    char *name = {};
+    QString name = {};
     int hide = {};
     int min_victim_position = {}; /* Position of victim */
 
     /* No argument was supplied */
-    char *char_no_arg = {};
-    char *others_no_arg = {};
+    QString char_no_arg = {};
+    QString others_no_arg = {};
 
     /* An argument was there, and a victim was found */
-    char *char_found = {}; /* if nullptr, read no further, ignore args */
-    char *others_found = {};
-    char *vict_found = {};
+    QString char_found = {}; /* if nullptr, read no further, ignore args */
+    QString others_found = {};
+    QString vict_found = {};
 
     /* An argument was there, but no victim was found */
-    char *not_found = {};
+    QString not_found = {};
 
     /* The victim turned out to be the character */
-    char *char_auto = {};
-    char *others_auto = {};
+    QString char_auto = {};
+    QString others_auto = {};
 };
 
 #endif
