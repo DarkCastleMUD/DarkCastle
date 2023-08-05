bd42f9cc8a8568a9c93d0be266c920c582d1aa5e
Jared H. Hudson <426967+jhhudso@users.noreply.github.com>
2023-08-05 12:30:51 -0500

    reformat

--- src/fount.cpp
+++ src/fount.cpp
@@ -29,92 +29,84 @@ extern "C"
 extern CWorld world;
 
 /*************************************************************************
-* Figures out if a fountain is present in the room                       *
-*************************************************************************/
+ * Figures out if a fountain is present in the room                       *
+ *************************************************************************/
 
-int FOUNTAINisPresent (Character *ch)
+int FOUNTAINisPresent(Character *ch)
 {
   class Object *tmp;
   bool found = false;
 
   for (tmp = world[ch->in_room].contents;
        tmp != nullptr && !found;
-       tmp = tmp->next_content) {
-    if((tmp->obj_flags.type_flag == ITEM_FOUNTAIN)  && CAN_SEE_OBJ(ch, tmp)) {
-       return(true);
+       tmp = tmp->next_content)
+  {
+    if ((tmp->obj_flags.type_flag == ITEM_FOUNTAIN) && CAN_SEE_OBJ(ch, tmp))
+    {
+      return (true);
     }
   }
-  return(false);
-
+  return (false);
 }
 
 /************************************************************************
-*  Fill skins and any other drink containers.                           *
-************************************************************************/
+ *  Fill skins and any other drink containers.                           *
+ ************************************************************************/
 
 int do_fill(Character *ch, char *argument, int cmd)
 {
   char buf[MAX_STRING_LENGTH];
   class Object *to_obj;
-  void name_to_drinkcon(class Object *obj, int type);
-  
+  void name_to_drinkcon(class Object * obj, int type);
+
   one_argument(argument, buf);
 
-  if(!*buf) /* No arguments */
-    {
-      act("What do you want to fill?",ch,0,0,TO_CHAR, 0);
-      return 1;
-    }
-  
-  if(!(to_obj = get_obj_in_list_vis(ch,buf,ch->carrying)))
-    {
-      act("You can't find it!",ch,0,0,TO_CHAR, 0);
-      return 1;
-    }
-
-  if(FOUNTAINisPresent(ch)) {
-
-  if(to_obj->obj_flags.type_flag!=ITEM_DRINKCON)
+  if (!*buf) /* No arguments */
   {
-    act("You can't pour anything into that.",ch,0,0,TO_CHAR, 0);
+    act("What do you want to fill?", ch, 0, 0, TO_CHAR, 0);
     return 1;
   }
 
-    if((to_obj->obj_flags.value[1]!=0)&&
-       (to_obj->obj_flags.value[2]!=0))
-      {
-    act("There is already another liquid in it!",ch,0,0,TO_CHAR, 0);
+  if (!(to_obj = get_obj_in_list_vis(ch, buf, ch->carrying)))
+  {
+    act("You can't find it!", ch, 0, 0, TO_CHAR, 0);
     return 1;
-      }
+  }
 
-    if(!(to_obj->obj_flags.value[1]<to_obj->obj_flags.value[0]))
-      {
-    act("There is no room for more.",ch,0,0,TO_CHAR, 0);
-    return 1;
-      }
+  if (FOUNTAINisPresent(ch))
+  {
 
-    act ("You fill $p!",ch,to_obj,0,TO_CHAR, 0);
+    if (to_obj->obj_flags.type_flag != ITEM_DRINKCON)
+    {
+      act("You can't pour anything into that.", ch, 0, 0, TO_CHAR, 0);
+      return 1;
+    }
+
+    if ((to_obj->obj_flags.value[1] != 0) &&
+        (to_obj->obj_flags.value[2] != 0))
+    {
+      act("There is already another liquid in it!", ch, 0, 0, TO_CHAR, 0);
+      return 1;
+    }
+
+    if (!(to_obj->obj_flags.value[1] < to_obj->obj_flags.value[0]))
+    {
+      act("There is no room for more.", ch, 0, 0, TO_CHAR, 0);
+      return 1;
+    }
+
+    act("You fill $p!", ch, to_obj, 0, TO_CHAR, 0);
 
     /* First same type liq. */
-    to_obj->obj_flags.value[2]=0;
-    
+    to_obj->obj_flags.value[2] = 0;
+
     /* Then how much to pour */
-    to_obj->obj_flags.value[1]=to_obj->obj_flags.value[0];
-
-
-    
-  } else {
-    act("There is no fountain here!",ch,0,0,TO_CHAR, 0);
+    to_obj->obj_flags.value[1] = to_obj->obj_flags.value[0];
   }
-  
+  else
+  {
+    act("There is no fountain here!", ch, 0, 0, TO_CHAR, 0);
+  }
+
   return 1;
 }
-
-
-
-
-
-
-
-
-
