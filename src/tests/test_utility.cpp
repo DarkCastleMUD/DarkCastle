96dd1c482dc09ca5a0c6fe153b74a63c8b2574d3
Jared H. Hudson <426967+jhhudso@users.noreply.github.com>
2024-05-11 16:02:51 -0500

    add more tests for nocolor_strlen()

--- src/tests/test_utility.cpp
+++ src/tests/test_utility.cpp
@@ -8,10 +8,22 @@ using namespace std::literals;
 #define STRING_LITERAL1 "$00$11$22$33$44$55$66$77$88$99$II$LL$**$RR$BB$$"
 #define STRING_LITERAL2 "$0$1$2$3$4$5$6$7$8$9$I$L$*$R$B"
 #define STRING_LITERAL3 ""
+#define STRING_LITERAL4 "$"
+#define STRING_LITERAL5 "test"
+#define STRING_LITERAL6 "$z$>$;"
 class UtilityTest : public QObject
 {
     Q_OBJECT
+
+    enum class VariableType
+    {
+        C_STRING,
+        STD_STRING,
+        QSTRING
+    };
+
 private slots:
+
     void test_double_dollars_qstring()
     {
         QString source = QStringLiteral("abc$def$12");
@@ -19,20 +31,41 @@ private slots:
         QCOMPARE(destination, QStringLiteral("abc$$def$$12"));
     }
 
-    void test_nocolor_strlen_qstring()
+    void test_nocolor_strlen_data()
     {
-        QCOMPARE(nocolor_strlen(QStringLiteral(STRING_LITERAL1)), 16);
-        QCOMPARE(nocolor_strlen(QStringLiteral(STRING_LITERAL2)), 0);
-        QCOMPARE(nocolor_strlen(QStringLiteral(STRING_LITERAL3)), 0);
-        QCOMPARE(nocolor_strlen(QStringLiteral("")), 0);
+        QTest::addColumn<VariableType>("type");
+        QTest::addColumn<QString>("string_literal");
+        QTest::addColumn<size_t>("expected");
+
+        QTest::newRow("C string test1") << VariableType::C_STRING << STRING_LITERAL1 << 16UL;
+        QTest::newRow("C string test2") << VariableType::C_STRING << STRING_LITERAL2 << 0UL;
+        QTest::newRow("C string test3") << VariableType::C_STRING << STRING_LITERAL3 << 0UL;
+        QTest::newRow("C string test4") << VariableType::C_STRING << STRING_LITERAL4 << 1UL;
+        QTest::newRow("C string test5") << VariableType::C_STRING << STRING_LITERAL5 << 4UL;
+        QTest::newRow("C string test6") << VariableType::C_STRING << STRING_LITERAL6 << 6UL;
+
+        QTest::newRow("QString test1") << VariableType::QSTRING << QStringLiteral(STRING_LITERAL1) << 16UL;
+        QTest::newRow("QString test2") << VariableType::QSTRING << QStringLiteral(STRING_LITERAL2) << 0UL;
+        QTest::newRow("QString test3") << VariableType::QSTRING << QStringLiteral(STRING_LITERAL3) << 0UL;
+        QTest::newRow("QString test4") << VariableType::QSTRING << QStringLiteral(STRING_LITERAL4) << 1UL;
+        QTest::newRow("QString test5") << VariableType::QSTRING << QStringLiteral(STRING_LITERAL5) << 4UL;
+        QTest::newRow("QString test6") << VariableType::QSTRING << QStringLiteral(STRING_LITERAL6) << 6UL;
     }
 
-    void test_nocolor_strlen_c()
+    void test_nocolor_strlen()
     {
-        QCOMPARE(nocolor_strlen(STRING_LITERAL1), 16);
-        QCOMPARE(nocolor_strlen(STRING_LITERAL2), 0);
-        QCOMPARE(nocolor_strlen(STRING_LITERAL3), 0);
-        QCOMPARE(nocolor_strlen(nullptr), 0);
+        QFETCH(VariableType, type);
+        QFETCH(QString, string_literal);
+        QFETCH(size_t, expected);
+
+        if (type == VariableType::C_STRING)
+        {
+            QCOMPARE(nocolor_strlen(string_literal), expected);
+        }
+        else if (type == VariableType::QSTRING)
+        {
+            QCOMPARE(nocolor_strlen(QString(string_literal)), expected);
+        }
     }
 
     void test_str_dup0()
@@ -44,6 +77,7 @@ private slots:
     void test_str_dup()
     {
         QCOMPARE(strlen(str_dup(STRING_LITERAL1)), strlen(STRING_LITERAL1));
+        // causes expected crash
         // QCOMPARE(str_dup(nullptr), nullptr);
     }
 };
