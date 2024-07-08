01dc90d740f3455299ab19192dbcfa96f9812bbd
Jared H. Hudson <426967+jhhudso@users.noreply.github.com>
2024-07-07 20:35:40 -0500

    applying endless loop bugfix earlier to replaceString

--- src/utility.cpp
+++ src/utility.cpp
@@ -2744,10 +2744,9 @@ QString replaceString(QString message, QString find, QString replace)
   if (find == replace)
     return message;
 
-  qsizetype find_length = find.length();
-  while (qsizetype j = message.indexOf(find) != -1)
+  while (message.indexOf(find) != -1)
   {
-    message.replace(j, find_length, replace);
+    message.replace(message.indexOf(find), find.length(), replace);
   }
   return message;
 }
