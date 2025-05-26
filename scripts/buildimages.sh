8ca31ad719bdfd3c90615721a27bedd25861aabe
Jared H. Hudson <426967+jhhudso@users.noreply.github.com>
2025-05-26 01:10:51 -0500

    script to build both devel and prod runtime images

---
+++ scripts/buildimages.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+export DCTMPDIR=$(mktemp -d)
+podman build --file Dockerfile --tag darkcastle-devel --volume "${DCTMPDIR}:/hostdir"
+podman build -f Dockerfile.prod --tag darkcastle-prod --volume ${DCTMPDIR}:/hostdir
+podman image save localhost/darkcastle-prod -o build/darkcastle-prod.image
+xzip --compress build/darkcastle-prod.image > build/darkcastle-prod.image.xz
