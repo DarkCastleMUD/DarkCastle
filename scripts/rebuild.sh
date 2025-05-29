0616dd84cd886f7991dc073055652205e0791eb7
Jared H. Hudson <426967+jhhudso@users.noreply.github.com>
2025-05-28 22:09:50 -0500

    rebuilt task now runs rebuild.sh script

---
+++ scripts/rebuild.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+NINJA_BUILD="$(ninja -n -C build)"
+echo "$NINJA_BUILD"
+if [[ ! "$NINJA_BUILD_STATE" =~ "ninja: no work to do." ]]; then
+    if ninja -C build; then
+        build/testDC test_legacy_prompts
+        if pidof dcastle; then
+            rebootdc.sh
+        fi
+    fi
+fi
+true
