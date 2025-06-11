4fffd1e91509d6a731873a048c6af229a1b8b9dc
Jared H. Hudson <426967+jhhudso@users.noreply.github.com>
2025-06-11 14:32:57 -0500

    commenting test for now

--- scripts/rebuild.sh
+++ scripts/rebuild.sh
@@ -3,7 +3,7 @@ NINJA_BUILD="$(ninja -n -C build)"
 echo "$NINJA_BUILD"
 if [[ ! "$NINJA_BUILD_STATE" =~ "ninja: no work to do." ]]; then
     if ninja -C build; then
-        build/testDC test_immortal_object_commands
+        #build/testDC
         if pidof dcastle; then
             rebootdc.sh
         fi
