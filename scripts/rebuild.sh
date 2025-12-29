383d5941ed9acee0d8202956c743ce1c96125218
Jared H. Hudson <426967+jhhudso@users.noreply.github.com>
2025-12-29 16:22:21 -0600

    cmake presets created for debug and release

--- scripts/rebuild.sh
+++ scripts/rebuild.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-NINJA_BUILD="$(ninja -n -C build)"
+NINJA_BUILD="$(ninja -n -C build/default)"
 echo "$NINJA_BUILD"
 if [[ ! "$NINJA_BUILD_STATE" =~ "ninja: no work to do." ]]; then
-    if ninja -C build; then
+    if ninja -C build/default; then
         #build/testDC
         if pidof dcastle; then
             rebootdc.sh
