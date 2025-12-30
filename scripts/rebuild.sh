33b3cd6f268d3d73813dd26206876dc56a423875
Jared H. Hudson <426967+jhhudso@users.noreply.github.com>
2025-12-29 20:14:06 -0600

    update to use preset debug instead of default

--- scripts/rebuild.sh
+++ scripts/rebuild.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-NINJA_BUILD="$(ninja -n -C build/default)"
+NINJA_BUILD="$(cmake --build --preset debug)"
 echo "$NINJA_BUILD"
 if [[ ! "$NINJA_BUILD_STATE" =~ "ninja: no work to do." ]]; then
-    if ninja -C build/default; then
+    if cmake --build --preset debug; then
         #build/testDC
         if pidof dcastle; then
             rebootdc.sh
