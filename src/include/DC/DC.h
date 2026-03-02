83de55c7c174896b40a2b9694a245d7b3f6f44c8
Jared H. Hudson <426967+jhhudso@users.noreply.github.com>
2026-03-01 21:34:43 -0600

    moved class index_data to Index.h

--- src/include/DC/DC.h
+++ src/include/DC/DC.h
@@ -115,9 +115,9 @@ typedef QList<QString> hints_t;
 #include "DC/Database.h"
 #include "DC/Command.h"
 #include "DC/shop.h"
+#include "DC/Index.h"
 
 class Connection;
-class index_data;
 class clan_data;
 
 using special_function = int (*)(Character *, class Object *, cmd_t, const char *, Character *);
@@ -143,23 +143,6 @@ typedef std::map<vnum_t, special_function> special_function_list_t;
 typedef QMap<zone_t, Zone> zones_t;
 
 /* element in monster and object index-tables   */
-class index_data
-{
-public:
-  void vnum(vnum_t v) { vnum_ = v; }
-  [[nodiscard]] vnum_t vnum(void) const { return vnum_; }
-  quint64 qty{};                                                                           /* number of existing units of ths mob/obj */
-  int (*non_combat_func)(Character *, class Object *, cmd_t, const char *, Character *){}; // non Combat special proc
-  int (*combat_func)(Character *, class Object *, cmd_t, const char *, Character *){};     // combat special proc
-  void *item{};                                                                            /* the mobile/object itself                 */
-
-  struct mob_prog_data *mobprogs{};
-  mob_prog_data *mobspec{};
-  int progtypes{};
-
-private:
-  vnum_t vnum_{}; /* virt number of ths mob/obj           */
-};
 
 class World
 {
