c8e9e5531bc621f0099667448f73ed642a1ebcc3
Jared H. Hudson <426967+jhhudso@users.noreply.github.com>
2026-03-09 11:29:49 -0500

    update build script to use cmake presets

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,20 +1,7 @@
 #!/bin/bash
-rm -rf build
-if [[ "$1" =~ prod.* ]]; then
-    echo production build
-    cmake -B build -DCMAKE_BUILD_TYPE=RelWithDebInfo \
-                   -DCMAKE_CXX_COMPILER="/usr/bin/g++-15" \
-                   -DCMAKE_EXE_LINKER_FLAGS="-fuse-ld=mold" \
-                   -DCMAKE_SHARED_LINKER_FLAGS="-fuse-ld=mold" \
-                   -GNinja
-    ninja -C build package
-    find build -name \*rpm -exec rpm --addsign {} \;
+if [[ "$1" =~ rel.* ]]; then
+    cmake --workflow --fresh --preset release
+    find build/release/ -name \*rpm -exec rpm --addsign {} \;
 else
-    echo debug build
-    cmake -B build -DCMAKE_BUILD_TYPE=Debug \
-                   -DCMAKE_CXX_COMPILER="/usr/bin/g++-15" \
-                   -DCMAKE_EXE_LINKER_FLAGS="-fuse-ld=mold" \
-                   -DCMAKE_SHARED_LINKER_FLAGS="-fuse-ld=mold" \
-                   -GNinja
-    ninja -C build
+    cmake --workflow --fresh --preset debug
 fi
