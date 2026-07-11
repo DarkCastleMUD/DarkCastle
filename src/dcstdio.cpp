ed37aaa80f38a9565c3321da9624284afa6fc0d0
Jared H. Hudson <426967+jhhudso@users.noreply.github.com>
2026-07-11 12:12:01 -0500

    added dc_fseek,dc_ftell,dc_clearerr,dc_feof,dc_ferror,dc_fread,dc_fwrite,dc_getline, dc_getdelim,dc_fputc,dc_putc,dc_fputs

--- src/dcstdio.cpp
+++ src/dcstdio.cpp
@@ -5,10 +5,10 @@
 void close_file(std::FILE *stream)
 {
   if (stream)
-    std::fclose(stream);
+    fclose(stream);
 }
 
-FILEPtr dc_fopen(const QString &path, const QString &modes)
+FILEPtr dc_fopen(QString path, QString modes)
 {
   auto stream = fopen(qPrintable(path), qPrintable(modes));
   return FILEPtr(stream, close_file);
@@ -45,4 +45,64 @@ int dc_fscanf(FILEPtr stream, QString format, ...)
   auto result = vfscanf(stream.data(), qPrintable(format), ap);
   va_end(ap);
   return result;
+}
+
+int dc_fseek(FILEPtr stream, long offset, int whence)
+{
+  return fseek(stream.data(), offset, whence);
+}
+
+long dc_ftell(FILEPtr stream)
+{
+  return ftell(stream.data());
+}
+
+void dc_clearerr(FILEPtr stream)
+{
+  clearerr(stream.data());
+}
+
+int dc_feof(FILEPtr stream)
+{
+  return feof(stream.data());
+}
+
+int dc_ferror(FILEPtr stream)
+{
+  return ferror(stream.data());
+}
+
+size_t dc_fread(void *ptr, size_t size, size_t n, FILEPtr stream)
+{
+  return fread(ptr, size, n, stream.data());
+}
+
+size_t dc_fwrite(const void *ptr, size_t size, size_t n, FILEPtr stream)
+{
+  return fwrite(ptr, size, n, stream.data());
+}
+
+ssize_t dc_getline(char **lineptr, size_t *n, FILEPtr stream)
+{
+  return getline(lineptr, n, stream.data());
+}
+
+ssize_t dc_getdelim(char **lineptr, size_t *n, int delim, FILEPtr stream)
+{
+  return getdelim(lineptr, n, delim, stream.data());
+}
+
+int dc_fputc(int c, FILEPtr stream)
+{
+  return fputc(c, stream.data());
+}
+
+int dc_putc(int c, FILEPtr stream)
+{
+  return putc(c, stream.data());
+}
+
+int dc_fputs(const char *s, FILEPtr stream)
+{
+  return fputs(s, stream.data());
 }
--- src/include/DC/dcstdio.h
+++ src/include/DC/dcstdio.h
@@ -3,9 +3,21 @@
 
 using FILEPtr = QSharedPointer<std::FILE>;
 
-FILEPtr dc_fopen(const QString &path, const QString &mode);
-int dc_fprintf(FILEPtr stream, const QString &format, ...);
+FILEPtr dc_fopen(QString path, QString mode);
+int dc_fprintf(FILEPtr stream, QString format, ...);
 char *dc_fgets(char *s, int size, FILEPtr stream);
 int dc_fgetc(FILEPtr stream);
 int dc_ungetc(int c, FILEPtr stream);
-int dc_fscanf(FILEPtr stream, const QString &format, ...);
+int dc_fscanf(FILEPtr stream, QString format, ...);
+int dc_fseek(FILEPtr stream, long offset, int whence);
+long dc_ftell(FILEPtr stream);
+void dc_clearerr(FILEPtr stream);
+int dc_feof(FILEPtr stream);
+int dc_ferror(FILEPtr stream);
+size_t dc_fread(void *ptr, size_t size, size_t n, FILEPtr stream);
+size_t dc_fwrite(const void *ptr, size_t size, size_t n, FILEPtr stream);
+ssize_t dc_getline(char **lineptr, size_t *n, FILEPtr stream);
+ssize_t dc_getdelim(char **lineptr, size_t *n, int delim, FILEPtr stream);
+int dc_fputc(int c, FILEPtr stream);
+int dc_putc(int c, FILEPtr stream);
+int dc_fputs(const char *s, FILEPtr stream);
