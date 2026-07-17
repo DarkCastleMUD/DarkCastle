e906eef870941bf6513f1f9acc7d36bafb2a1b9e
Jared H. Hudson <426967+jhhudso@users.noreply.github.com>
2026-07-17 00:29:09 -0500

    added class world_file_list_item here, added read_next_file/new_file_item/indexFileName to sub-classes obj_index_data and mob_index_data

--- src/include/DC/Index.h
+++ src/include/DC/Index.h
@@ -1,10 +1,26 @@
 #pragma once
 #include <stdint.h>
-#include <qnumeric.h>
+#include <QSet>
+#include <QList>
+#include <QString>
 #include <DC/Command.h>
+#include "DC/dcstdio.h"
 
 typedef quint64 vnum_t;
+QString read_next_filename(FILEPtr stream, QString directoryName);
 
+class world_file_list_item
+{
+public:
+  QString filename{};
+  vnum_t firstnum{};
+  vnum_t lastnum{};
+  QMap<vnum_t, bool> vnums_{};
+  int32_t flags{};
+};
+using world_file_list_itemPtr = QSharedPointer<class world_file_list_item>;
+using world_file_list_t = QList<world_file_list_itemPtr>;
+world_file_list_itemPtr new_w_file_item(QString filename, world_file_list_t &list);
 class index_data
 {
 public:
@@ -24,11 +40,17 @@ private:
 class obj_index_data : public index_data
 {
 public:
+  static QString read_next_file(FILEPtr stream);
+  static world_file_list_itemPtr new_file_item(QString filename);
+  static const QString indexFilename;
   Object *item{};
 };
 
 class mob_index_data : public index_data
 {
 public:
+  static QString read_next_file(FILEPtr stream);
+  static world_file_list_itemPtr new_file_item(QString filename);
+  static const QString indexFilename;
   Character *mob{};
 };
